17669bdcb3e4fc6f9c116d080e4259211f2b964c
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-12-29 13:17:05 -0800

    Add `--fullJson` to saf view:summary (#16)

--- src/utils/threshold.ts
+++ src/utils/threshold.ts
@@ -1,6 +1,7 @@
-import {ContextualizedProfile, ControlStatus, Severity} from 'inspecjs'
+import {ContextualizedControl, ContextualizedProfile, ControlStatus, Severity} from 'inspecjs'
 import {StatusHash, ThresholdValues} from '../types/threshold'
 import _ from 'lodash'
+import {ControlDescription} from 'inspecjs/lib/generated_parsers/v_1_0/exec-json'
 
 export const severityTargetsObject = {
   critical: ['passed.critical.min', 'passed.critical.max', 'failed.critical.min', 'failed.critical.max', 'skipped.critical.min', 'skipped.critical.max', 'error.critical.min', 'error.critical.max'],
@@ -146,4 +147,108 @@ export function getControlIdMap(profile: ContextualizedProfile, thresholds?: Thr
     _.set(thresholds, path, [...existingData, control.data.id])
   }
   return thresholds
+}
+
+function getDescriptionContentsOrUndefined(label: string, descriptions?: ControlDescription[] | {[key: string]: any} | null) {
+  let found
+  if (descriptions) {
+    // eslint-disable-next-line unicorn/no-array-for-each
+    descriptions.forEach((description: any) => {
+      if (description.label === label) {
+        found = description.data
+      }
+    })
+  }
+  return found
+}
+
+function cklControlStatus(control: ContextualizedControl, for_summary?: boolean): 'Not_Applicable' | 'Profile_Error' | 'Open' | 'NotAFinding' | 'Not_Reviewed' {
+  const statuses = control.hdf.segments?.map(segment => segment.status)
+  if (control.data.impact === 0) {
+    return 'Not_Applicable'
+  }
+  if (statuses?.includes('error') || (statuses?.length === 0 && for_summary)) {
+    return 'Profile_Error'
+  }
+  if (statuses?.includes('failed')) {
+    return 'Open'
+  }
+  if (statuses?.includes('passed')) {
+    return 'NotAFinding'
+  }
+  return 'Not_Reviewed'
+}
+
+function controlFindingDetails(control: {message: string[]}, controlCKLStatus: 'Not_Applicable' | 'Profile_Error' | 'Open' | 'NotAFinding' | 'Not_Reviewed') {
+  control.message.sort()
+  switch (controlCKLStatus) {
+  case 'Open':
+    return `One or more of the automated tests failed or was inconclusive for the control \n\n ${control.message.join('\n')}`
+  case 'NotAFinding':
+    return `All Automated tests passed for the control \n\n ${control.message.join('\n')}`
+  case 'Not_Reviewed':
+    return `Automated test skipped due to known accepted condition in the control : \n\n${control.message.join('\n')}`
+  case 'Not_Applicable':
+    return `Justification: \n ${control.message.join('\n')}`
+  default:
+    return 'No test available or some test errors occurred for this control'
+  }
+}
+
+export function extractControlSummariesBySeverity(profile: ContextualizedProfile): Record<string, Record<string, Record<string, string | string[] | number | undefined>>> {
+  const result: Record<string, Record<string, Record<string, string | string[] | number | undefined>>>  = {
+    failed: {},
+    passed: {},
+    no_impact: {},
+    skipped: {},
+    error: {},
+  }
+  for (const c of profile.contains.filter(control => control.extendedBy.length === 0)) {
+    const control = c.root
+    const status: ControlStatus = control.hdf.status
+    const extracted: Record<string, string | string[] | undefined> & {message: string[]}  = {
+      vuln_num: control.data.id,
+      rule_title: control.data.title || undefined,
+      vuln_discuss: control.data.desc || undefined,
+      severity: control.hdf.severity,
+      gid: control.data.tags.gid,
+      group_title: control.data.tags.gtitle,
+      rule_id: control.data.tags.rid,
+      rule_ver: control.data.tags.stig_id,
+      cci_ref: control.data.tags.cci,
+      nist: (control.data.tags.nist || []).join(' '),
+      check_content: getDescriptionContentsOrUndefined('check', control.data.descriptions),
+      fix_text: getDescriptionContentsOrUndefined('fix', control.data.descriptions),
+      impact: control.data.impact.toString() || undefined,
+      profile_name: profile.data.name,
+      profile_shasum: profile.data.sha256,
+      status: control.hdf.segments?.map(segment => segment.status),
+      message: [],
+      control_status: cklControlStatus(control, true),
+    }
+    control.hdf.segments?.forEach(segment => {
+      switch (segment.status) {
+      case 'skipped':
+        extracted.message.push(`SKIPPED -- Test: ${segment.code_desc}\nMessage: ${segment.skip_message}\n`)
+        break
+      case 'failed':
+        extracted.message.push(`FAILED -- Test: ${segment.code_desc}\nMessage: ${segment.message}\n`)
+        break
+      case 'passed':
+        extracted.message.push(`PASS -- ${segment.code_desc}\n`)
+        break
+      case 'error':
+        extracted.message.push(`PROFILE_ERROR -- Test: ${segment.code_desc}\nMessage: ${segment.code_desc}\n`)
+        break
+      default:
+        break
+      }
+    })
+    if (control.data.impact === 0) {
+      extracted.message.push(`NOT_APPLICABLE -- Description: ${control.data.desc}\n\n`)
+    }
+    extracted.finding_details = controlFindingDetails(extracted, cklControlStatus(control, true))
+    result[reverseStatusName(status)][control.data.id] = extracted
+  }
+  return result
 }
