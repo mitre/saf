46f0918382c8a204788f59d2d9b69ed971acbeda
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-01-25 22:54:55 -0500

    Support --targetURL or --method/--host/--user/--password, delete container after use, promisify websocket connection

--- src/commands/scan/scan.ts
+++ src/commands/scan/scan.ts
@@ -10,16 +10,19 @@ export default class Scan extends Command {
   static flags = {
     help: flags.help({char: 'h'}),
     profile: flags.integer({char: 'p', description: 'ID of the profile you would like to run'}),
-    target: flags.string({char: 't', description: 'Standard target URI (e.g ssh://root:password@192.168.1.1)'}),
+    targetURL: flags.string({char: 't', description: 'Standard target URI (e.g ssh://root:password@192.168.1.1)'}),
     method: flags.string({char: 'm', default: 'ssh', options: ['ssh', 'winrm'], description: 'Transport method to evaluate profiles'}),
-    username: flags.string({char: 'u', description: 'Username to authenticate with'}),
-    password: flags.string({char: 'P', description: 'Password to authenticate with'}),
+    host: flags.string({char: 'H', description: 'Target host to scan'}),
+    username: flags.string({char: 'u', description: 'Username to authenticate with', required: false}),
+    password: flags.string({char: 'P', description: 'Password to authenticate with', required: false}),
+    output: flags.string({char: 'o', description: 'Output HDF file', default: 'output.json'})
   }
 
-  async makeRequest(endpoint: string, method: 'GET' | 'POST', data?: Record<string, unknown>) {
+  async makeRequest(endpoint: string, method: 'GET' | 'POST' | 'DELETE', data?: Record<string, unknown>): any {
     const request = got(`unix:/var/run/docker.sock:${endpoint}`, {
       method: method,
       json: data,
+      throwHttpErrors: false
     })
 
     return request.json()
@@ -38,30 +41,47 @@ export default class Scan extends Command {
   }
 
   async handleContainerWebsocketConnection(id: string) {
-    const client = new ws(`ws+unix:///var/run/docker.sock:/containers/${id}/attach/ws?stream=1&stdout=1&stdin=1&logs=1`)
+    return new Promise((resolve, reject) => {
+      const client = new ws(`ws+unix:///var/run/docker.sock:/containers/${id}/attach/ws?stream=1&stdout=1&stdin=1&logs=1`)
 
-    client.on('open', () => {
-      console.log(`Connected to container ${id}`)
-    })
+      client.on('open', () => {
+        console.log(`Connected to container ${id}`)
+      })
 
-    client.on('message', (data: Buffer) => {
-      console.log(data.toString())
-    })
+      client.on('message', (data: Buffer) => {
+        console.log(data.toString())
+      })
 
-    client.on('error', error => {
-      console.error(error)
-    })
+      client.on('error', error => {
+        reject(error)
+      })
 
-    client.on('close', () => {
-      console.log(`Disconnected from container ${id}`)
+      client.on('close', () => {
+        resolve(id)
+      })
     })
+    
   }
 
-  async createContainer(image: string, args: string[]): Promise<{Id: string; Warnings: string[]}> {
+  async createContainer(image: string, args: string[]): Promise<string> {
     return this.makeRequest('/containers/create', 'POST', {
       Image: image,
       Cmd: args,
-    }) as Promise<{Id: string; Warnings: string[]}>
+      HostConfig: {
+        Binds: [
+          `${process.cwd()}:/share`
+        ]
+      }
+    }).then((fulfilment: {Id: string}) => {
+      console.log(`Created container ${fulfilment.Id}`)
+      return fulfilment.Id
+    })
+  }
+
+  async deleteContainer(id: string) {
+    return this.makeRequest(`/containers/${id}`, 'DELETE').then(() => {
+      console.log(`Deleted container ${id}`)
+    })
   }
 
   async getContainerInfo(id: string) {
@@ -69,11 +89,19 @@ export default class Scan extends Command {
   }
 
   async startContainer(id: string) {
-    return this.makeRequest(`/containers/${id}/start`, 'POST')
+    return this.makeRequest(`/containers/${id}/start`, 'POST').then(() => {
+      console.log(`Started container ${id}`)
+    })
   }
 
-  async getContainerLogs(name: string) {
-    return this.makeRequest(`/v1.24/containers/${name}/logs`, 'GET')
+  buildTargetURL(method: string, host: string, username: string, password: string) {
+    if (method === 'ssh') {
+      return ['--target', `${method}://${username}:${password}@${host}`]
+    } else if (method === 'winrm') {
+      return ['--backend', 'winrm', '--user', username, '--password', password, '--host', host]
+    } else {
+       throw new Error("Invalid transport method")
+     }
   }
 
   async run() {
@@ -91,11 +119,24 @@ export default class Scan extends Command {
         const [name, url] = profile
         console.log(`Selected profile ${name} (${url})`)
         // Create a CINC Auditor container
-        const containerId = await this.createContainer('cincproject/auditor', ['exec', '--help']).then(containerResponse => containerResponse.Id)
+        let containerId;
+        if(flags.targetURL) {
+          containerId = await this.createContainer('cincproject/auditor',
+            ["exec", url, "--log-level", "debug", "--reporter", `json:${flags.output}`, "--target", flags.targetURL]
+          )
+        } else if (flags.method && flags.host && flags.username && flags.password) {
+          containerId = await this.createContainer('cincproject/auditor',
+            ["exec", url, "--log-level", "debug", "--reporter", `json:${flags.output}`, ...this.buildTargetURL(flags.method, flags.host, flags.username, flags.password)]
+          )
+        } else {
+          throw new Error("Please provide a target url (--target) or use --method, --host, --username, and --password ")
+        }    
         // Start the container
         await this.startContainer(containerId)
         // Get the logs from the container
         await this.handleContainerWebsocketConnection(containerId)
+        // Delete the container
+        await this.deleteContainer(containerId)
       } else {
         console.error(`Invalid profile selected, please choose a number 1-${availableProfiles.length - 2}`)
       }
