c75a86519937a9743804f5bf38b7330b9cd95cdc
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-01-26 12:35:28 -0500

    Add tests for spreadsheet2inspec, standardize .d.ts for all types files

---
+++ test/commands/convert/spreadsheet2inspec.test.ts
@@ -0,0 +1,17 @@
+import {expect, test} from '@oclif/test'
+import * as tmp from 'tmp'
+import path from 'path'
+import fs from 'fs'
+import _ from 'lodash'
+
+describe('Test spreadsheet2inspec', () => {
+  const tmpobj = tmp.dirSync({unsafeCleanup: true})
+
+  test
+  .stdout()
+  .command(['convert:spreadsheet2inspec', '-i', path.resolve('./test/sample_data/csv/input/Ubuntu.csv'), '-o', `${tmpobj.name}/Ubuntu`, '--format', 'disa'])
+  .it('Has the same number of controls in the CSV as generated - Ubuntu', () => {
+    const fileCount = fs.readdirSync(`${tmpobj.name}/Ubuntu/controls/`).length
+    expect(fileCount).to.eql(194)
+  })
+})
--- test/commands/convert/xccdf2inspec.test.ts
+++ test/commands/convert/xccdf2inspec.test.ts
@@ -16,7 +16,7 @@ describe('Test xccdf2inspec', () => {
     .it(`Has the same number of controls in the stig as generated - ${file}`, () => {
       const parsedXML: DisaStig = convertEncodedXmlIntoJson(fs.readFileSync(path.resolve('./test/sample_data/xccdf/stigs', file), 'utf-8'))
       const fileCount = fs.readdirSync(`${tmpobj.name}/${file}/controls/`).length
-      expect(parsedXML.Benchmark.Group.length).to.eql(fileCount)
+      expect(fileCount).to.eql(parsedXML.Benchmark.Group.length)
     })
   })
 })
