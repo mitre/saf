20133b395ab3b58b2cafd8da6f5f91b460231558
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-02-02 15:20:56 -0500

    Add generate:ckl_metadata

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,4 +1,5 @@
 import {getInstalledPathSync} from 'get-installed-path'
+import {ContextualizedEvaluation} from 'inspecjs'
 import {ExecJSONProfile} from 'inspecjs/lib/generated_parsers/v_1_0/exec-json'
 import _ from 'lodash'
 import path from 'path'
@@ -80,12 +81,10 @@ export function extractValueViaPathOrNumber(typeOfPathOrNumber: string, pathOrNu
   }
 }
 
-export function getProfileInfo(file: ExtendedEvaluationFile): string {
+export function getProfileInfo(evaluation: ContextualizedEvaluation, fileName: string): string {
   let result = ''
-  const profile: ExecJSONProfile = _.get(file, 'evaluation.data.profiles[0]')
-  if (file.fileName) {
-    result += `File Name: ${file.fileName}\n`
-  }
+  const profile: ExecJSONProfile = _.get(evaluation, 'data.profiles[0]')
+  result += `File Name: ${fileName}\n`
   if (profile.version) {
     result += `Version: ${profile.version}\n`
   }
