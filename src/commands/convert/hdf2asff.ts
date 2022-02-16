c37de40aa9dad490b1599d220483669acbd16f83
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2022-02-16 10:48:46 -0500

    Update hdf2asff.ts

--- src/commands/convert/hdf2asff.ts
+++ src/commands/convert/hdf2asff.ts
@@ -13,9 +13,7 @@ export default class HDF2ASFF extends Command {
 
   static description = 'Translate a Heimdall Data Format JSON file into AWS Security Findings Format JSON file(s) and/or upload to AWS Security Hub'
 
-  static examples = ['saf convert:hdf2asff -i rhel7-scan_02032022A.json -a 123456789 -r us-east-1 -t rhel7_example_host -o rhel7.asff', 
-                     'saf convert:hdf2asff -i rds_mysql_i123456789scan_03042022A.json -a 987654321 -r us-west-1 -t Instance_i123456789 -u',
-                     'saf convert:hdf2asff -i snyk_acme_project5_hdf_04052022A.json -a 2143658798 -r us-east-1 -t acme_project5 -o snyk_acme_project5 -u' ]
+  static examples = ['saf convert:hdf2asff -i rhel7-scan_02032022A.json -a 123456789 -r us-east-1 -t rhel7_example_host -o rhel7.asff','saf convert:hdf2asff -i rds_mysql_i123456789scan_03042022A.json -a 987654321 -r us-west-1 -t Instance_i123456789 -u','saf convert:hdf2asff -i snyk_acme_project5_hdf_04052022A.json -a 2143658798 -r us-east-1 -t acme_project5 -o snyk_acme_project5 -u' ]
 
   static flags = {
     help: flags.help({char: 'h'}),
