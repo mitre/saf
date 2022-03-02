4ca03a117f2544a706d54ce4f684ba297857f095
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-03-02 13:49:29 -0600

    Add winston logger
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

--- src/commands/convert/asff2hdf.ts
+++ src/commands/convert/asff2hdf.ts
@@ -2,6 +2,7 @@ import {Command, flags} from '@oclif/command'
 import fs from 'fs'
 import {ASFFMapper as Mapper} from '@mitre/hdf-converters'
 import {checkSuffix} from '../../utils/global'
+import { createWinstonLogger } from '../../utils/logging'
 
 export default class ASFF2HDF extends Command {
   static usage = 'convert:asff2hdf -i <asff-finding-json> [--securityhub <standard-1-json> ... <standard-n-json>] -o <hdf-scan-results-json>'
@@ -16,16 +17,26 @@ export default class ASFF2HDF extends Command {
     input: flags.string({char: 'i', required: true}),
     securityhub: flags.string({required: false, multiple: true}),
     output: flags.string({char: 'o', required: true}),
+    logLevel: flags.string({char: 'L', required: false, default: 'info', options: ['info', 'warn', 'debug', 'verbose']})
   }
 
   async run() {
     const {flags} = this.parse(ASFF2HDF)
+    const logger = createWinstonLogger('asff2hdf', flags.logLevel)
     let securityhub
     if (flags.securityhub) {
+      logger.verbose('Reading ASFF standards')
       securityhub = flags.securityhub.map(file => fs.readFileSync(file, 'utf-8'))
     }
-
-    const converter = new Mapper(fs.readFileSync(flags.input, 'utf-8'), securityhub)
-    fs.writeFileSync(checkSuffix(flags.output), JSON.stringify(converter.toHdf()))
+    
+    logger.verbose(`Reading ASFF file: ${flags.input}`)
+    const inputData = fs.readFileSync(flags.input, 'utf-8')
+    logger.info('Starting conversion from ASFF to HDF')
+    const converter = new Mapper(inputData, securityhub)
+    const converted = JSON.stringify(converter.toHdf())
+    logger.info('Converted ASFF to HDF')
+    logger.info(`Writing HDF to: ${checkSuffix(flags.output)}`)
+    fs.writeFileSync(checkSuffix(flags.output), converted)
+    logger.verbose(`Converted HDF successfully written to ${checkSuffix(flags.output)}`)
   }
 }
