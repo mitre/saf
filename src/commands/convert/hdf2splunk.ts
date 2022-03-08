6152ce0693a005f745dd1e2edb601ca90146ef42
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-03-08 11:17:34 -0500

    [WIP] Splunk Updates for 2.6.14

--- src/commands/convert/hdf2splunk.ts
+++ src/commands/convert/hdf2splunk.ts
@@ -11,26 +11,27 @@ export default class HDF2Splunk extends Command {
   static flags = {
     help: flags.help({char: 'h'}),
     input: flags.string({char: 'i', required: true, description: 'Input HDF file'}),
-    host: flags.string({char: 'H', required: true, description: 'Splunk Host'}),
-    port: flags.integer({char: 'P', required: false, default: 8088}),
-    protocol: flags.string({char: 'p', required: false, default: 'https'}),
-    token: flags.string({char: 't', required: true, description: 'Your HTTP event collector token'}),
-    index: flags.string({char: 'I', required: false, description: 'Splunk index to import data into (if none provided then Splunk-default will be used)'}),
+    host: flags.string({char: 'H', required: true, description: 'Splunk Hostname or IP'}),
+    port: flags.integer({char: 'P', required: false, description: 'Splunk management port (also known as the Universal Forwarder port)', default: 8089}),
+    scheme: flags.string({char: 's', required: false, description: 'HTTP Scheme used for communication with splunk', default: 'https', options: ['http', 'https']}),
+    username: flags.string({char: 'u', required: true, description: 'Your Splunk username'}),
+    password: flags.string({char: 'p', required: true, description: 'Your Splunk password'}),
+    index: flags.string({char: 'I', required: true, description: 'Splunk index to import HDF data into'}),
   }
 
-  static examples = ['saf convert:hdf2splunk -i rhel7-results.json -H 127.0.0.1 -P 8088 -t YOUR-HEC-TOKEN']
+  static examples = ['saf convert:hdf2splunk -i rhel7-results.json -H 127.0.0.1 -u admin -p Valid_password! -I hdf']
 
   async run() {
     const {flags} = this.parse(HDF2Splunk)
     const inputFile = JSON.parse(fs.readFileSync(flags.input, 'utf-8'))
-    const guid = await new FromHDFToSplunkMapper(inputFile).toSplunk({
+    const guid = new FromHDFToSplunkMapper(inputFile).toSplunk({
       host: flags.host,
       port: flags.port,
-      protocol: flags.protocol,
-      token: flags.token,
+      scheme: flags.scheme as 'http' | 'https',  // Types as defined by flags
+      username: flags.username,
+      password: flags.password,
       index: flags.index,
+      insecure: true, // The Splunk SDK's requestOptions somehow broke on release of the mapper, this will be fixed in mitre/heimdall2#2675
     }, convertFullPathToFilename(flags.input))
-
-    console.log(`Uploaded into splunk successfully, to find this data search for:\n\n\t meta.guid="${guid}"`)
   }
 }
