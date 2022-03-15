6ccf4413255453ead8fc1d85ee77db8c1bdca1cb
Camden Moors <cmoors@mitre.org>
2022-03-15 15:22:25 -0400

    Disable eslint rule for promise that does not use resolved value

--- test/commands/convert/jfrog_xray2hdf.test.ts
+++ test/commands/convert/jfrog_xray2hdf.test.ts
@@ -25,6 +25,7 @@ describe('Test jfrog_xray', () => {
 })
 
 function delay(ms: number) {
+  // eslint-disable-next-line no-promise-executor-return
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
