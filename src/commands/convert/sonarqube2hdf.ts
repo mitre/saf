cbc4a1beb0d38044fdb3f1c5c6c5dad081bff235
blake <blake.burton@caemilusa.com>
2022-06-01 07:01:06 -0400

    added pull request arg to sonarqube converter. added arg description as well

--- src/commands/convert/sonarqube2hdf.ts
+++ src/commands/convert/sonarqube2hdf.ts
@@ -4,24 +4,25 @@ import {SonarQubeResults as Mapper} from '@mitre/hdf-converters'
 import {checkSuffix} from '../../utils/global'
 
 export default class Sonarqube2HDF extends Command {
-  static usage = 'convert sonarqube2hdf -n <sonar_project_key> -u <http://your.sonar.instance:9000> --auth <your-sonar-api-key> -b <target_branch> -o <hdf-scan-results-json>'
+  static usage = 'convert sonarqube2hdf -n <sonar_project_key> -u <http://your.sonar.instance:9000> --auth <your-sonar-api-key> [ -b <target_branch> || -p <pull-request-id> ] -o <hdf-scan-results-json>'
 
   static description = 'Pull SonarQube vulnerabilities for the specified project name and optional branch name from an API and convert into a Heimdall Data Format JSON file'
 
-  static examples = ['saf convert sonarqube2hdf -n sonar_project_key -u http://sonar:9000 --auth YOUR_API_KEY -b my_branch -o scan_results.json']
+  static examples = ['saf convert sonarqube2hdf -n sonar_project_key -u http://sonar:9000 --auth YOUR_API_KEY [ -b my_branch || -p 123 ]-o scan_results.json']
 
   static flags = {
     help: Flags.help({char: 'h'}),
     auth: Flags.string({char: 'a', required: true}),
     projectKey: Flags.string({char: 'n', required: true}),
     url: Flags.string({char: 'u', required: true}),
-    branch: Flags.string({char: 'b', required: false}),
+    branch: Flags.string({char: 'b', required: false, exclusive: ['pullRequestID'], description: "Requires Sonarqube Developer Edition or above"}),
+    pullRequestID: Flags.string({char: 'p', required: false, exclusive: ['branch'], description: "Requires Sonarqube Developer Edition or above"}),
     output: Flags.string({char: 'o', required: true}),
   }
 
   async run() {
     const {flags} = await this.parse(Sonarqube2HDF)
-    const converter = new Mapper(flags.url, flags.projectKey, flags.auth, flags.branch)
+    const converter = new Mapper(flags.url, flags.projectKey, flags.auth, flags.branch, flags.pullRequestID)
     fs.writeFileSync(checkSuffix(flags.output), JSON.stringify(await converter.toHdf()))
   }
 }
