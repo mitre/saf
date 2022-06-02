178a7dd9f85b7eb1b8c83ca0b32219d9d2c5f149
blake <blake.burton@caemilusa.com>
2022-06-02 07:12:58 -0400

    added to sonarqube converter

--- src/commands/convert/sonarqube2hdf.ts
+++ src/commands/convert/sonarqube2hdf.ts
@@ -6,7 +6,7 @@ import {checkSuffix} from '../../utils/global'
 export default class Sonarqube2HDF extends Command {
   static usage = 'convert sonarqube2hdf -n <sonar_project_key> -u <http://your.sonar.instance:9000> --auth <your-sonar-api-key> [ -b <target_branch> || -p <pull-request-id> ] -o <hdf-scan-results-json>'
 
-  static description = 'Pull SonarQube vulnerabilities for the specified project name and optional branch name from an API and convert into a Heimdall Data Format JSON file'
+  static description = 'Pull SonarQube vulnerabilities for the specified project name and optional branch or pull/merge request ID name from an API and convert into a Heimdall Data Format JSON file'
 
   static examples = ['saf convert sonarqube2hdf -n sonar_project_key -u http://sonar:9000 --auth YOUR_API_KEY [ -b my_branch || -p 123 ]-o scan_results.json']
 
@@ -15,8 +15,8 @@ export default class Sonarqube2HDF extends Command {
     auth: Flags.string({char: 'a', required: true}),
     projectKey: Flags.string({char: 'n', required: true}),
     url: Flags.string({char: 'u', required: true}),
-    branch: Flags.string({char: 'b', required: false, exclusive: ['pullRequestID'], description: "Requires Sonarqube Developer Edition or above"}),
-    pullRequestID: Flags.string({char: 'p', required: false, exclusive: ['branch'], description: "Requires Sonarqube Developer Edition or above"}),
+    branch: Flags.string({char: 'b', required: false, exclusive: ['pullRequestID'], description: 'Requires Sonarqube Developer Edition or above'}),
+    pullRequestID: Flags.string({char: 'p', required: false, exclusive: ['branch'], description: 'Requires Sonarqube Developer Edition or above'}),
     output: Flags.string({char: 'o', required: true}),
   }
 
