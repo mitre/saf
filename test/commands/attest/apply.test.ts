1f7db48e6591067b8a3bdbe7cc476aa79610e6d3
Camden Moors <cmoors@mitre.org>
2022-06-08 12:13:58 -0400

    Add JSON attestation test

---
+++ test/commands/attest/apply.test.ts
@@ -0,0 +1,45 @@
+import {expect, test} from '@oclif/test'
+import * as tmp from 'tmp'
+import path from 'path'
+import fs from 'fs'
+import {ExecJSON} from 'inspecjs'
+import _ from 'lodash'
+
+export function omitHDFChangingFields(
+  input: Partial<ExecJSON.Execution> & {profiles: ExecJSON.Profile[]},
+) {
+  return {
+    ..._.omit(input, ['version', 'platform.release', 'profiles[0].sha256']),
+    profiles: input.profiles.map(profile => {
+      return {
+        ...profile,
+        controls: profile.controls.map(control => {
+          return {
+            ...control,
+            results: control.results.map(result => {
+              return {
+                ..._.omit(result, 'start_time'),
+                message: result.message?.replace(/Updated:.*\n/g, ''),
+              }
+            }),
+          }
+        }),
+      }
+    }),
+  }
+}
+
+describe('Test attest apply', () => {
+  const tmpobj = tmp.dirSync({unsafeCleanup: true})
+
+  test
+  .stdout()
+  .stderr()
+  .command(['attest apply', '-i', path.resolve('./test/sample_data/HDF/input/triple_overlay_profile_example.json'), path.resolve('./test/sample_data/attestations/rhel7-attestations.json'), '-o', `${tmpobj.name}/triple_overlay_example_json-attestation.json`])
+  .it('Sucessfully applies a JSON attestations file', () => {
+    const output = JSON.parse(fs.readFileSync(`${tmpobj.name}/triple_overlay_example_json-attestation.json`, 'utf-8'))
+    const expected = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/attestations/triple_overlay_example_json-attestation.json'), 'utf-8'))
+
+    expect(omitHDFChangingFields(output)).to.eql(omitHDFChangingFields(expected))
+  })
+})
