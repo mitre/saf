e574b27cb68e0c898f8cb2eefb0d88ccd704b48f
Camden Moors <cmoors@mitre.org>
2022-06-09 08:53:51 -0400

    Add MinMaxTotal tests

--- test/commands/validate/threshold.test.ts
+++ test/commands/validate/threshold.test.ts
@@ -13,7 +13,7 @@ describe('Test validate threshold', () => {
     ),
     '--templateFile',
     path.resolve(
-      './test/sample_data/HDF/input/triple_overlay_profile_example.json.counts.good',
+      './test/sample_data/HDF/input/thresholds/triple_overlay_profile_example.json.counts.good.yml',
     ),
   ])
   .it('Validate threshold test - Triple Overlay Valid Counts', ctx => {
@@ -32,7 +32,7 @@ describe('Test validate threshold', () => {
     ),
     '--templateFile',
     path.resolve(
-      './test/sample_data/HDF/input/triple_overlay_profile_example.json.counts.bad.total',
+      './test/sample_data/HDF/input/thresholds/triple_overlay_profile_example.json.counts.bad.total.yml',
     ),
   ])
   .catch(error => {
@@ -42,7 +42,7 @@ describe('Test validate threshold', () => {
     'Validate threshold test - Triple Overlay Invalid Total Counts',
     ctx => {
       expect(ctx.stdout).to.equal('')
-    },
+    }
   )
 
   test
@@ -56,16 +56,29 @@ describe('Test validate threshold', () => {
     ),
     '--templateFile',
     path.resolve(
-      './test/sample_data/HDF/input/triple_overlay_profile_example.json.counts.bad.compliance',
+      './test/sample_data/HDF/input/thresholds/triple_overlay_profile_example.json.counts.bad.compliance.yml',
     ),
   ])
   .catch(error => {
     expect(error.message).to.equal('Overall compliance minimum was not satisfied')
   })
-  .it(
-    'Validate threshold test - Triple Overlay Compliance',
+  .it('Validate threshold test - Triple Overlay Compliance',
     ctx => {
       expect(ctx.stdout).to.equal('')
     },
   )
+
+  test.stdout().stderr()
+  .command(['validate threshold',
+  '-i',
+  path.resolve(
+    './test/sample_data/HDF/input/triple_overlay_profile_example.json',
+  ),
+  '--templateFile',
+  path.resolve(
+    './test/sample_data/HDF/input/thresholds/triple_overlay_profile_example.json.counts.totalMinMax.yml',
+  )]).catch((error) => {
+    expect(error.message).to.equal('passed.total.max: Recieved 19 > Expected 18')
+  })
+  .it('Validate threshold minMaxTotal - Triple Overlay Compliance')
 })
