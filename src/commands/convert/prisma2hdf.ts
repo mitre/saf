56123e81a32870d3c27e33b01ef66638be86a199
Rlin232 <rlin@mitre.org>
2022-06-21 12:08:09 -0700

    Ran linter
    Signed-off-by: Rlin232 <rlin@mitre.org>

--- src/commands/convert/prisma2hdf.ts
+++ src/commands/convert/prisma2hdf.ts
@@ -1,7 +1,7 @@
-import { Command, Flags } from '@oclif/core'
+import {Command, Flags} from '@oclif/core'
 import fs from 'fs'
-import { PrismaMapper as Mapper } from '@mitre/hdf-converters'
-import { checkSuffix } from '../../utils/global'
+import {PrismaMapper as Mapper} from '@mitre/hdf-converters'
+import {checkSuffix} from '../../utils/global'
 
 export default class Prisma2HDF extends Command {
   static usage = 'convert prisma2hdf -i, --input=CSV -o, --output=OUTPUT'
@@ -11,13 +11,13 @@ export default class Prisma2HDF extends Command {
   static examples = ['saf convert prisma2hdf -i prismacloud-report.csv -o output-hdf-name.json']
 
   static flags = {
-    help: Flags.help({ char: 'h' }),
-    input: Flags.string({ char: 'i', required: true }),
-    output: Flags.string({ char: 'o', required: true }),
+    help: Flags.help({char: 'h'}),
+    input: Flags.string({char: 'i', required: true}),
+    output: Flags.string({char: 'o', required: true}),
   }
 
   async run() {
-    const { flags } = await this.parse(Prisma2HDF)
+    const {flags} = await this.parse(Prisma2HDF)
 
     const converter = new Mapper(fs.readFileSync(flags.input, 'utf8'))
     fs.writeFileSync(checkSuffix(flags.output), JSON.stringify(converter.toHdf()))
