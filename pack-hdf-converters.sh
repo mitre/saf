d0d039abf2d89465a430e08ed219944349dac131
George Dias <george.m.dias.ctr@army.mil>
2022-06-24 16:06:48 -0500

    Move mac content to the sh file

---
+++ pack-hdf-converters.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+original_dir= $(pwd)
+
+cd ${npm_config_heimdall:-../heimdall2}
+cd libs/hdf-converters
+
+git switch ${npm_config_branch:-master}
+git fetch 
+git pull
+
+CYPRESS_INSTALL_BINARY=0 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true yarn install
+yarn pack
+
+cd $original_dir
+npm i 
+npm i ${npm_config_heimdall:-../heimdall2}/libs/hdf-converters/mitre-hdf-converters-v*.tgz && yarn prepack
