63cba65cf2278c230f6990808f97ac1723b999c3
Rlin232 <rlin@mitre.org>
2022-06-24 14:28:57 -0700

    Modified shell script
    Signed-off-by: Rlin232 <rlin@mitre.org>

--- pack-hdf-converters.sh
+++ pack-hdf-converters.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-original_dir= $(pwd)
+ORIGINAL=$PWD
 
-cd ${npm_config_heimdall:-../heimdall2}
+cd "${npm_config_heimdall:-../heimdall2}"
 cd libs/hdf-converters
 
-git switch ${npm_config_branch:-master}
-git fetch 
+git switch "${npm_config_branch:-master}"
+git fetch
 git pull
 
 CYPRESS_INSTALL_BINARY=0 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true yarn install
 yarn pack
 
-cd $original_dir
-npm i 
-npm i ${npm_config_heimdall:-../heimdall2}/libs/hdf-converters/mitre-hdf-converters-v*.tgz && yarn prepack
+cd "$ORIGINAL"
+npm i
+npm i "${npm_config_heimdall:-../heimdall2}/libs/hdf-converters/mitre-hdf-converters-v"*".tgz" && yarn prepack
