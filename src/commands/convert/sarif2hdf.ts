369cf06b56c9381621a7aca15cbd9c089ee28f4b
charleshu-mitre <chu@mitre.org>
2022-06-27 15:51:44 -0400

    Added base for raw flag
    Signed-off-by: charleshu-mitre <chu@mitre.org>

--- src/commands/convert/sarif2hdf.ts
+++ src/commands/convert/sarif2hdf.ts
@@ -4,22 +4,23 @@ import {SarifMapper as Mapper} from '@mitre/hdf-converters'
 import {checkSuffix} from '../../utils/global'
 
 export default class Sarif2HDF extends Command {
-  static usage = 'convert sarif2hdf -i, --input=JSON -o, --output=OUTPUT'
+  static usage = 'convert sarif2hdf -i, --input=JSON -o, --output=OUTPUT -r, --raw=BOOLEAN'
 
   static description = 'Translate a SARIF JSON file into a Heimdall Data Format JSON file\nSARIF level to HDF impact Mapping:\nSARIF level error -> HDF impact 0.7\nSARIF level warning -> HDF impact 0.5\nSARIF level note -> HDF impact 0.3\nSARIF level none -> HDF impact 0.1\nSARIF level not provided -> HDF impact 0.1 as default'
 
-  static examples = ['saf convert sarif2hdf -i sarif-results.json -o output-hdf-name.json']
+  static examples = ['saf convert sarif2hdf -i sarif-results.json -o output-hdf-name.json -r=true']
 
   static flags = {
     help: Flags.help({char: 'h'}),
     input: Flags.string({char: 'i', required: true}),
     output: Flags.string({char: 'o', required: true}),
+    raw: Flags.boolean({char: 'r', required: false}),
   }
 
   async run() {
     const {flags} = await this.parse(Sarif2HDF)
 
-    const converter = new Mapper(fs.readFileSync(flags.input, 'utf8'))
+    const converter = new Mapper(fs.readFileSync(flags.input, 'utf8'), flags.raw)
     fs.writeFileSync(checkSuffix(flags.output), JSON.stringify(converter.toHdf()))
   }
 }
