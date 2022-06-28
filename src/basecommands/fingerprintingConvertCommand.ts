22d24cb255798d2648d9f3b14a55cbb06d821234
Rlin232 <rlin@mitre.org>
2022-06-28 11:46:43 -0700

    Added prisma to generic convert command and fixed nessus in generic convert
    Signed-off-by: Rlin232 <rlin@mitre.org>

--- src/basecommands/fingerprintingConvertCommand.ts
+++ src/basecommands/fingerprintingConvertCommand.ts
@@ -1,13 +1,13 @@
-import {Command, Flags} from '@oclif/core'
+import { Command, Flags } from '@oclif/core'
 import * as fs from 'fs'
-import {fingerprint} from '@mitre/hdf-converters'
-import {OutputFlags, Input} from '@oclif/core/lib/interfaces/parser'
-import {convertFullPathToFilename} from '../utils/global'
+import { fingerprint } from '@mitre/hdf-converters'
+import { OutputFlags, Input } from '@oclif/core/lib/interfaces/parser'
+import { convertFullPathToFilename } from '../utils/global'
 
 export default abstract class FingerprintingConvertCommand extends Command {
   static flags = {
-    input: Flags.string({char: 'i', required: true, description: 'Input results set file'}),
-    output: Flags.string({char: 'o', required: true, description: 'Output results sets'}),
+    input: Flags.string({ char: 'i', required: true, description: 'Input results set file' }),
+    output: Flags.string({ char: 'o', required: true, description: 'Output results sets' }),
   }
 
   protected additionalFlags = {};
@@ -18,38 +18,39 @@ export default abstract class FingerprintingConvertCommand extends Command {
 
   getFlagsForInputFile(path: string) {
     if (path) {
-      FingerprintingConvertCommand.detectedType = fingerprint({data: fs.readFileSync(path, 'utf8'), filename: convertFullPathToFilename(path)})
+      FingerprintingConvertCommand.detectedType = fingerprint({ data: fs.readFileSync(path, 'utf8'), filename: convertFullPathToFilename(path) })
       switch (FingerprintingConvertCommand.detectedType) {
-      case 'asff':
-        return {
-          securityhub: Flags.string({
-            required: false,
-            multiple: true,
-            description: 'Additional input files to provide context that an ASFF file needs such as the CIS AWS Foundations or AWS Foundational Security Best Practices documents (in ASFF compliant JSON form)',
-          }),
-        }
-      case 'zap':
-        return {
-          name: Flags.string({
-            char: 'n',
-            required: true,
-          }),
-        }
-      case 'burp':
-      case 'dbProtect':
-      case 'fortify':
-      case 'jfrog':
-      case 'nessus':
-      case 'netsparker':
-      case 'nikto':
-      case 'sarif':
-      case 'scoutsuite':
-      case 'snyk':
-      case 'twistlock':
-      case 'xccdf':
-        return {}
-      default:
-        throw new Error(`Unknown filetype provided: ${path}`)
+        case 'asff':
+          return {
+            securityhub: Flags.string({
+              required: false,
+              multiple: true,
+              description: 'Additional input files to provide context that an ASFF file needs such as the CIS AWS Foundations or AWS Foundational Security Best Practices documents (in ASFF compliant JSON form)',
+            }),
+          }
+        case 'zap':
+          return {
+            name: Flags.string({
+              char: 'n',
+              required: true,
+            }),
+          }
+        case 'burp':
+        case 'dbProtect':
+        case 'fortify':
+        case 'jfrog':
+        case 'nessus':
+        case 'netsparker':
+        case 'nikto':
+        case 'prisma':
+        case 'sarif':
+        case 'scoutsuite':
+        case 'snyk':
+        case 'twistlock':
+        case 'xccdf':
+          return {}
+        default:
+          throw new Error(`Unknown filetype provided: ${path}`)
       }
     }
 
@@ -57,7 +58,7 @@ export default abstract class FingerprintingConvertCommand extends Command {
   }
 
   async init() {
-    const {flags} = await this.parse(this.constructor as Input<typeof FingerprintingConvertCommand.flags>)
+    const { flags } = await this.parse(this.constructor as Input<typeof FingerprintingConvertCommand.flags>)
     this.parsedFlags = flags
   }
 }
