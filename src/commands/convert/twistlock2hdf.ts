3efb453cf3381320f3affeb5d819f378b300b7f5
charleshu-mitre <chu@mitre.org>
2022-07-05 10:06:58 -0400

    Added withRaw flag to Twistlock convert command
    Signed-off-by: charleshu-mitre <chu@mitre.org>

--- src/commands/convert/twistlock2hdf.ts
+++ src/commands/convert/twistlock2hdf.ts
@@ -4,24 +4,24 @@ import {TwistlockMapper as Mapper} from '@mitre/hdf-converters'
 import {checkSuffix} from '../../utils/global'
 
 export default class Twistlock2HDF extends Command {
-  static usage = 'convert twistlock2hdf -i <twistlock-json> -o <hdf-scan-results-json>'
+  static usage = 'convert twistlock2hdf -i, --input=<twistlock-json> -o, output=<hdf-scan-results-json> -w, --withRaw'
 
   static description = 'Translate a Twistlock CLI output file into an HDF results set'
 
-  static examples = ['saf convert twistlock2hdf -i twistlock.json -o output-hdf-name.json']
+  static examples = ['saf convert twistlock2hdf -i twistlock.json -o output-hdf-name.json -w']
 
   static flags = {
     help: Flags.help({char: 'h'}),
     input: Flags.string({char: 'i', required: true, description: 'Input Twistlock file'}),
     output: Flags.string({char: 'o', required: true, description: 'Output HDF file'}),
+    withRaw: Flags.boolean({char: 'w', required: false}),
   }
 
   async run() {
     const {flags} = await this.parse(Twistlock2HDF)
     const input = fs.readFileSync(flags.input, 'utf8')
 
-    const converter = new Mapper(input)
+    const converter = new Mapper(input, flags.withRaw)
     fs.writeFileSync(checkSuffix(flags.output), JSON.stringify(converter.toHdf()))
   }
 }
-
