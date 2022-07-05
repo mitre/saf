23dc5c9e66d0f4033433f26e9df459c8b6396168
charleshu-mitre <chu@mitre.org>
2022-07-05 09:58:27 -0400

    Added withRaw flag to Sarif convert command
    Signed-off-by: charleshu-mitre <chu@mitre.org>

--- src/commands/convert/sarif2hdf.ts
+++ src/commands/convert/sarif2hdf.ts
@@ -4,17 +4,17 @@ import {SarifMapper as Mapper} from '@mitre/hdf-converters'
 import {checkSuffix} from '../../utils/global'
 
 export default class Sarif2HDF extends Command {
-  static usage = 'convert sarif2hdf -i, --input=JSON -o, --output=OUTPUT -r, --raw=BOOLEAN'
+  static usage = 'convert sarif2hdf -i, --input=JSON -o, --output=OUTPUT -w, --withRaw'
 
   static description = 'Translate a SARIF JSON file into a Heimdall Data Format JSON file\nSARIF level to HDF impact Mapping:\nSARIF level error -> HDF impact 0.7\nSARIF level warning -> HDF impact 0.5\nSARIF level note -> HDF impact 0.3\nSARIF level none -> HDF impact 0.1\nSARIF level not provided -> HDF impact 0.1 as default'
 
-  static examples = ['saf convert sarif2hdf -i sarif-results.json -o output-hdf-name.json -r=true']
+  static examples = ['saf convert sarif2hdf -i sarif-results.json -o output-hdf-name.json -w']
 
   static flags = {
     help: Flags.help({char: 'h'}),
     input: Flags.string({char: 'i', required: true}),
     output: Flags.string({char: 'o', required: true}),
-    raw: Flags.boolean({char: 'r', required: false}),
+    withRaw: Flags.boolean({char: 'w', required: false}),
   }
 
   async run() {
