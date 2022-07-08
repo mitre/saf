8b8a9864309ac2dacc5371b8e352f036926e1e24
Rlin232 <rlin@mitre.org>
2022-07-08 15:56:15 -0700

    Added shell attributes to exit on error to the .sh
    Signed-off-by: Rlin232 <rlin@mitre.org>

--- pack-hdf-converters.sh
+++ pack-hdf-converters.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
 ORIGINAL=$PWD
 echo $ORIGINAL
 
