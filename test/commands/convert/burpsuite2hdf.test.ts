685a0050ddea8f8238ebf4ddb1eec6dc80f251a7
Rlin232 <rlin@mitre.org>
2022-07-08 12:04:38 -0700

    Ensured file names of tests remained accurate
    Signed-off-by: Rlin232 <rlin@mitre.org>

--- test/commands/convert/burpsuite2hdf.test.ts
+++ test/commands/convert/burpsuite2hdf.test.ts
@@ -1,31 +1,31 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
 import * as tmp from 'tmp'
 import path from 'path'
 import fs from 'fs'
-import {omitHDFChangingFields} from '../utils'
+import { omitHDFChangingFields } from '../utils'
 
 describe('Test burpsuite', () => {
-  const tmpobj = tmp.dirSync({unsafeCleanup: true})
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true })
 
   test
-  .stdout()
-  .command(['convert burpsuite2hdf', '-i', path.resolve('./test/sample_data/burpsuite/sample_input_report/zero.webappsecurity.com.min'), '-o', `${tmpobj.name}/burpsuitetest.json`])
-  .it('hdf-converter output test', () => {
-    const test = JSON.parse(fs.readFileSync(`${tmpobj.name}/burpsuitetest.json`, 'utf8'))
-    const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/burpsuite/zero.webappsecurity.json'), 'utf8'))
-    expect(omitHDFChangingFields(test)).to.eql(omitHDFChangingFields(sample))
-  })
+    .stdout()
+    .command(['convert burpsuite2hdf', '-i', path.resolve('./test/sample_data/burpsuite/sample_input_report/zero.webappsecurity.com.min'), '-o', `${tmpobj.name}/burpsuitetest.json`])
+    .it('hdf-converter output test', () => {
+      const test = JSON.parse(fs.readFileSync(`${tmpobj.name}/burpsuitetest.json`, 'utf8'))
+      const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/burpsuite/burpsuite-hdf.json'), 'utf8'))
+      expect(omitHDFChangingFields(test)).to.eql(omitHDFChangingFields(sample))
+    })
 })
 
 describe('Test using space topic separator using burpsuite case', () => {
-  const tmpobj = tmp.dirSync({unsafeCleanup: true})
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true })
 
   test
-  .stdout()
-  .command(['convert burpsuite2hdf', '-i', path.resolve('./test/sample_data/burpsuite/sample_input_report/zero.webappsecurity.com.min'), '-o', `${tmpobj.name}/burpsuitetest.json`])
-  .it('hdf-converter output test', () => {
-    const test = JSON.parse(fs.readFileSync(`${tmpobj.name}/burpsuitetest.json`, 'utf8'))
-    const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/burpsuite/zero.webappsecurity.json'), 'utf8'))
-    expect(omitHDFChangingFields(test)).to.eql(omitHDFChangingFields(sample))
-  })
+    .stdout()
+    .command(['convert burpsuite2hdf', '-i', path.resolve('./test/sample_data/burpsuite/sample_input_report/zero.webappsecurity.com.min'), '-o', `${tmpobj.name}/burpsuitetest.json`])
+    .it('hdf-converter output test', () => {
+      const test = JSON.parse(fs.readFileSync(`${tmpobj.name}/burpsuitetest.json`, 'utf8'))
+      const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/burpsuite/burpsuite-hdf.json'), 'utf8'))
+      expect(omitHDFChangingFields(test)).to.eql(omitHDFChangingFields(sample))
+    })
 })
