c49d5900d6dba1f81b5da903332018601ea4d8cc
Rlin232 <rlin@mitre.org>
2022-07-08 12:11:00 -0700

    Removed use of wildcard imports
    Signed-off-by: Rlin232 <rlin@mitre.org>

--- test/commands/convert/convert.test.ts
+++ test/commands/convert/convert.test.ts
@@ -1,46 +1,46 @@
-import { expect, test } from '@oclif/test'
-import * as tmp from 'tmp'
+import {expect, test} from '@oclif/test'
+import tmp from 'tmp'
 import path from 'path'
 import fs from 'fs'
-import { omitHDFChangingFields } from '../utils'
-import { execSync } from 'child_process'
+import {omitHDFChangingFields} from '../utils'
+import {execSync} from 'child_process'
 
 describe('Test (generic) convert', () => {
-  const tmpobj = tmp.dirSync({ unsafeCleanup: true })
+  const tmpobj = tmp.dirSync({unsafeCleanup: true})
 
   test
-    .stdout()
-    .it('hdf-converter output test (asff)', () => {
-      execSync(
-        `node bin/run convert -i ./test/sample_data/asff/sample_input_report/asff_sample.json -o ${tmpobj.name}/asfftest`,
-      )
+  .stdout()
+  .it('hdf-converter output test (asff)', () => {
+    execSync(
+      `node bin/run convert -i ./test/sample_data/asff/sample_input_report/asff_sample.json -o ${tmpobj.name}/asfftest`,
+    )
 
-      const test = JSON.parse(fs.readFileSync(`${tmpobj.name}/asfftest/CIS AWS Foundations Benchmark v1.2.0.json`, 'utf8'))
-      const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/asff/asff-hdf.json'), 'utf8'))
-      expect(omitHDFChangingFields(test)).to.eql(omitHDFChangingFields(sample))
-    })
-
-  test
-    .stdout()
-    .it('hdf-converter output test (burpsuite)', () => {
-      execSync(
-        `node bin/run convert -i ./test/sample_data/burpsuite/sample_input_report/zero.webappsecurity.com.min -o ${tmpobj.name}/burpsuitetest.json`,
-      )
-
-      const test = JSON.parse(fs.readFileSync(`${tmpobj.name}/burpsuitetest.json`, 'utf8'))
-      const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/burpsuite/burpsuite-hdf.json'), 'utf8'))
-      expect(omitHDFChangingFields(test)).to.eql(omitHDFChangingFields(sample))
-    })
+    const test = JSON.parse(fs.readFileSync(`${tmpobj.name}/asfftest/CIS AWS Foundations Benchmark v1.2.0.json`, 'utf8'))
+    const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/asff/asff-hdf.json'), 'utf8'))
+    expect(omitHDFChangingFields(test)).to.eql(omitHDFChangingFields(sample))
+  })
 
   test
-    .stdout()
-    .it('hdf-converter output test (jfrog)', () => {
-      execSync(
-        `node bin/run convert -i ./test/sample_data/jfrog_xray/sample_input_report/jfrog_xray_sample.json -o ${tmpobj.name}/jfrogtest.json`,
-      )
+  .stdout()
+  .it('hdf-converter output test (burpsuite)', () => {
+    execSync(
+      `node bin/run convert -i ./test/sample_data/burpsuite/sample_input_report/zero.webappsecurity.com.min -o ${tmpobj.name}/burpsuitetest.json`,
+    )
 
-      const converted = JSON.parse(fs.readFileSync(`${tmpobj.name}/jfrogtest.json`, 'utf8'))
-      const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/jfrog_xray/jfrog-hdf.json'), 'utf8'))
-      expect(omitHDFChangingFields(converted)).to.eql(omitHDFChangingFields(sample))
-    })
+    const test = JSON.parse(fs.readFileSync(`${tmpobj.name}/burpsuitetest.json`, 'utf8'))
+    const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/burpsuite/burpsuite-hdf.json'), 'utf8'))
+    expect(omitHDFChangingFields(test)).to.eql(omitHDFChangingFields(sample))
+  })
+
+  test
+  .stdout()
+  .it('hdf-converter output test (jfrog)', () => {
+    execSync(
+      `node bin/run convert -i ./test/sample_data/jfrog_xray/sample_input_report/jfrog_xray_sample.json -o ${tmpobj.name}/jfrogtest.json`,
+    )
+
+    const converted = JSON.parse(fs.readFileSync(`${tmpobj.name}/jfrogtest.json`, 'utf8'))
+    const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/jfrog_xray/jfrog-hdf.json'), 'utf8'))
+    expect(omitHDFChangingFields(converted)).to.eql(omitHDFChangingFields(sample))
+  })
 })
