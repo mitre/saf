e6237a9756005ee9d36baa9f4864f89d508e72bd
Camden Moors <cmoors@mitre.org>
2022-07-20 11:16:53 -0400

    Update fingerprintingConvertCommand.ts

--- src/basecommands/fingerprintingConvertCommand.ts
+++ src/basecommands/fingerprintingConvertCommand.ts
@@ -17,47 +17,51 @@ export default abstract class FingerprintingConvertCommand extends Command {
   protected parsedFlags?: OutputFlags<typeof FingerprintingConvertCommand.flags>;
 
   getFlagsForInputFile(path: string) {
-    // If we're passed a folder just return {}
-    if (fs.lstatSync(path).isDirectory()) {
+    try {
+      if (path) {
+        // If we're passed a folder just return {}
+        if (fs.lstatSync(path).isDirectory()) {
+          return {}
+        }
+
+        FingerprintingConvertCommand.detectedType = fingerprint({data: fs.readFileSync(path, 'utf8'), filename: convertFullPathToFilename(path)})
+        switch (FingerprintingConvertCommand.detectedType) {
+        case 'asff':
+          return {
+            securityhub: Flags.string({
+              required: false,
+              multiple: true,
+              description: 'Additional input files to provide context that an ASFF file needs such as the CIS AWS Foundations or AWS Foundational Security Best Practices documents (in ASFF compliant JSON form)',
+            }),
+          }
+        case 'zap':
+          return {
+            name: Flags.string({
+              char: 'n',
+              required: true,
+            }),
+          }
+        case 'burp':
+        case 'dbProtect':
+        case 'fortify':
+        case 'jfrog':
+        case 'nessus':
+        case 'netsparker':
+        case 'nikto':
+        case 'prisma':
+        case 'sarif':
+        case 'scoutsuite':
+        case 'snyk':
+        case 'twistlock':
+        case 'xccdf':
+          return {}
+        }
+      }
+
+      return {}
+    } catch {
       return {}
     }
-
-    if (path) {
-      FingerprintingConvertCommand.detectedType = fingerprint({data: fs.readFileSync(path, 'utf8'), filename: convertFullPathToFilename(path)})
-      switch (FingerprintingConvertCommand.detectedType) {
-      case 'asff':
-        return {
-          securityhub: Flags.string({
-            required: false,
-            multiple: true,
-            description: 'Additional input files to provide context that an ASFF file needs such as the CIS AWS Foundations or AWS Foundational Security Best Practices documents (in ASFF compliant JSON form)',
-          }),
-        }
-      case 'zap':
-        return {
-          name: Flags.string({
-            char: 'n',
-            required: true,
-          }),
-        }
-      case 'burp':
-      case 'dbProtect':
-      case 'fortify':
-      case 'jfrog':
-      case 'nessus':
-      case 'netsparker':
-      case 'nikto':
-      case 'prisma':
-      case 'sarif':
-      case 'scoutsuite':
-      case 'snyk':
-      case 'twistlock':
-      case 'xccdf':
-        return {}
-      }
-    }
-
-    return {}
   }
 
   async init() {
