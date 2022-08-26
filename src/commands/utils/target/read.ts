62f5639397b580f08e063261f0965dddc58c1489
Amndeep Singh Mann <amann@mitre.org>
2022-08-26 00:30:06 -0400

    added target read and write - regex replace passthrough -> target
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

---
+++ src/commands/utils/target/read.ts
@@ -0,0 +1,31 @@
+import {Command, Flags} from '@oclif/core'
+import {ExecJSON} from 'inspecjs'
+import fs from 'fs'
+
+export default class ReadTarget extends Command {
+    static usage = 'target read -i <hdf-json> [-o <target-json>]'
+
+    static description = 'Read the `target` attribute in a given Heimdall Data Format JSON file and send it to stdout or write it to a file'
+
+    static examples = ['saf target read -i hdf.json -o target.json']
+
+    static flags = {
+      help: Flags.help({char: 'h'}),
+      input: Flags.string({char: 'i', required: true, description: 'An input HDF file'}),
+      output: Flags.string({char: 'o', description: 'An output `target` JSON file (otherwise the data is sent to stdout)'}),
+    }
+
+    async run() {
+      const {flags} = await this.parse(ReadTarget)
+
+      const input: ExecJSON.Execution & {target?: unknown} = JSON.parse(fs.readFileSync(flags.input, 'utf8'))
+
+      const target = input.target || {}
+
+      if (flags.output) {
+        fs.writeFileSync(flags.output, JSON.stringify(target, null, 2))
+      } else {
+        console.log(JSON.stringify(target, null, 2))
+      }
+    }
+}
---
+++ src/commands/utils/target/write.ts
@@ -0,0 +1,47 @@
+import {Command, Flags} from '@oclif/core'
+import {ExecJSON} from 'inspecjs'
+import fs from 'fs'
+
+export default class WriteTarget extends Command {
+    static usage = 'target write -i <input-hdf-json> (-f <input-target-json> | -d <target-json>) [-o <output-hdf-json>]'
+
+    static description = 'Overwrite the `target` attribute in a given Heimdall Data Format JSON file with the provided `target` JSON data'
+
+    static examples = ['saf target write -i hdf.json -d \'{"a": 5}\'']
+
+    static flags = {
+      help: Flags.help({char: 'h'}),
+      input: Flags.string({char: 'i', required: true, description: 'An input Heimdall Data Format file'}),
+      targetFile: Flags.string({char: 'f', exclusive: ['targetData'], description: 'An input target-data file (can contain any valid JSON); this flag or `targetData` must be provided'}),
+      targetData: Flags.string({char: 'd', exclusive: ['targetFile'], description: 'Input target-data (can be any valid JSON); this flag or `targetFile` must be provided'}),
+      output: Flags.string({char: 'o', description: 'An output Heimdall Data Format JSON file (otherwise the input file is overwritten)'}),
+    }
+
+    async run() {
+      const {flags} = await this.parse(WriteTarget)
+
+      const input: ExecJSON.Execution & {target?: unknown} = JSON.parse(fs.readFileSync(flags.input, 'utf8'))
+      const output: string = flags.output || flags.input
+
+      let target: unknown
+      if (flags.targetFile) {
+        try {
+          target = JSON.parse(fs.readFileSync(flags.targetFile, 'utf8'))
+        } catch (error: unknown) {
+          throw new Error(`Couldn't parse target data: ${error}`)
+        }
+      } else if (flags.targetData) {
+        try {
+          target = JSON.parse(flags.targetData)
+        } catch {
+          target = flags.targetData
+        }
+      } else {
+        throw new Error('One out of targetFile or targetData must be passed')
+      }
+
+      input.target = target
+
+      fs.writeFileSync(output, JSON.stringify(input, null, 2))
+    }
+}
