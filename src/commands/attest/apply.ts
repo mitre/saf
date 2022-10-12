239d5b2ee8c225ac5897956fa0811f4d0922a7a1
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-10-12 14:08:02 -0400

    Update apply.ts

--- src/commands/attest/apply.ts
+++ src/commands/attest/apply.ts
@@ -3,9 +3,9 @@ import {ExecJSON} from 'inspecjs'
 import {addAttestationToHDF, Attestation, parseXLSXAttestations} from '@mitre/hdf-converters'
 import _ from 'lodash'
 import yaml from 'yaml'
+import fs from 'fs'
 import path from 'path'
 import {convertFullPathToFilename} from '../../utils/global'
-import {createFolderIfNotExists, readFileURI, writeFileURI} from '../../utils/io'
 
 export default class ApplyAttestation extends Command {
     static usage = 'attest apply -i <input-hdf-json>... <attestation>... -o <output-hdf-path>'
@@ -32,7 +32,7 @@ export default class ApplyAttestation extends Command {
       for (const inputFile of flags.input) {
         let inputData
         try {
-          inputData = JSON.parse(await readFileURI(inputFile, 'utf8'))
+          inputData = JSON.parse(fs.readFileSync(inputFile, 'utf8'))
           if (Array.isArray(inputData) && inputData.length > 0 && _.get(inputData, '[0].control_id')) {
             // We have an attestations JSON
             attestations.push(...inputData)
@@ -48,10 +48,10 @@ export default class ApplyAttestation extends Command {
             process.exit(1)
           }
         } catch {
-          inputData = await readFileURI(inputFile, 'utf8')
+          inputData = fs.readFileSync(inputFile, 'utf8')
           if (inputFile.toLowerCase().endsWith('xlsx')) {
             // We have a spreadsheet
-            attestations.push(...(await parseXLSXAttestations(await readFileURI(inputFile, 'binary') as unknown as Uint8Array)))
+            attestations.push(...(await parseXLSXAttestations(fs.readFileSync(inputFile, null))))
           } else if (inputFile.toLowerCase().endsWith('yml') || inputFile.toLowerCase().endsWith('yaml')) {
             // We have a YAML
             attestations.push(...yaml.parse(inputData))
@@ -61,8 +61,8 @@ export default class ApplyAttestation extends Command {
         }
       }
 
-      if (Object.entries(executions).length > 1) {
-        createFolderIfNotExists(flags.output)
+      if (Object.entries(executions).length > 1 && !fs.existsSync(flags.output)) {
+        fs.mkdirSync(flags.output)
       }
 
       if (Object.keys(executions).length === 0) {
@@ -71,7 +71,11 @@ export default class ApplyAttestation extends Command {
 
       for (const [originalFilename, execution] of Object.entries(executions)) {
         const applied = addAttestationToHDF(execution, attestations)
-        await (Object.entries(executions).length <= 1 ? writeFileURI(flags.output, JSON.stringify(applied, null, 2)) : writeFileURI(path.join(flags.output, originalFilename), JSON.stringify(applied, null, 2)))
+        if (Object.entries(executions).length <= 1) {
+          fs.writeFileSync(flags.output, JSON.stringify(applied, null, 2))
+        } else {
+          fs.writeFileSync(path.join(flags.output, originalFilename), JSON.stringify(applied, null, 2))
+        }
       }
     }
 }
