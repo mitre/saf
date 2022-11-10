d54b2ab0387cfb35f4555fc280c7c6f4a03711fb
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-11-10 10:41:31 -0600

    fixed type errors in splunk, asff mappers, and checklist file
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

--- src/utils/checklist.ts
+++ src/utils/checklist.ts
@@ -63,7 +63,7 @@ export function getDetails(control: ContextualizedControl, profileName: string):
     checkText: control.hdf.descriptions.check || control.data.tags.check,
     fixText: control.hdf.descriptions.fix || control.data.tags.fix,
     profileName: profileName,
-    startTime: _.get(control, 'hdf.segments![0].start_time'),
+    startTime: _.get(control, 'hdf.segments![0].start_time', ''),
     targetKey: 0,
     uuidV4: v4(),
     ccis: control.data.tags.cci,
