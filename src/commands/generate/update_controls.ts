466366e376111b1119c4c6a6d342a8208b7e4607
George Dias <gdias@mitre.org>
2023-03-18 22:31:44 -0500

    finished update_controls first draft
    Signed-off-by: George Dias <gdias@mitre.org>

--- src/commands/generate/update_controls.ts
+++ src/commands/generate/update_controls.ts
@@ -1,28 +1,28 @@
 import fs from 'fs'
+import path from 'path'
 import {readdir} from 'fs/promises'
 import {Command, Flags} from '@oclif/core'
 import {processXCCDF} from '@mitre/inspec-objects'
 import {createWinstonLogger} from '../../utils/logging'
 import Profile from '@mitre/inspec-objects/lib/objects/profile'
-import path from 'path'
-
 
 export default class UpdateControls extends Command {
-
   static usage = '<%= command.id %> [ARGUMENTS]'
 
-  static description = 'Update the controls names of an existing InSpec profile with updated XCCDF guidance, old controls are place in oldControls directory'
+  static description = 'Update the control names for an existing InSpec profile with updated XCCDF guidance, old controls are saved by default'
 
   static flags = {
     help: Flags.help({char: 'h'}),
     xccdfXmlFile: Flags.string({char: 'X', required: true, description: 'The XCCDF XML file containing the new guidance - in the form of .xml file'}),
-    controlsDir: Flags.string({char: 'o', required: true, description: 'The InsPect profile controls directory containing the profiles to be updated'}),
-    backupControls: Flags.boolean({char: 'b', required: false, default: true, description: 'Create an oldControls ddirectory in the controls directory and save old controls there'}),
+    controlsDir: Flags.string({char: 'c', required: true, description: 'The InsPect profile controls directory containing the profiles to be updated'}),
+    controlPrefix: Flags.string({char: 'P', required: false, default: 'V', options: ['V', 'SV'], description: 'New control number prefix V or SV, default V'}),
+    backupControls: Flags.boolean({char: 'b', required: false, default: true, allowNo: true, description: 'Create an oldControls directory in the controls directory and save old controls there'}),
     logLevel: Flags.string({char: 'L', required: false, default: 'info', options: ['info', 'warn', 'debug', 'verbose']}),
   }
 
   static examples = [
-    'saf generate update_controls -X ./the_xccdf_guidance_file.xml  -o the_output_directory -o ./path_to_inspect_controls_directory -L debug',
+    'saf generate update_controls -X ./the_xccdf_guidance_file.xml  -c the_controls_directory -L debug',
+    'saf generate update_controls -X ./the_xccdf_guidance_file.xml  -c the_controls_directory --no-backupControls -P SV -L debug',
   ]
 
   async run(): Promise<any> {
@@ -55,105 +55,104 @@ export default class UpdateControls extends Command {
     }
 
     // Check if we have a controls folder
-    if (!fs.existsSync(flags.controlsDir)) {
-      throw new Error('Controls folder not specified or does not exist')
-    } else {
+    if (fs.existsSync(flags.controlsDir)) {
       logger.debug('Found controls directory')
-      fs.readdir(flags.controlsDir, function(err, files) {
+      fs.readdir(flags.controlsDir, function (err, files) {
         if (err) {
           logger.error(`ERROR: Checking in controls directory is empty, received: ${err.message}`)
           throw new Error(`Error checking controls directory, error: ${err.message}`)
-        } else {
-          if (!files.length) {
-            // directory appears to be empty
-            logger.error(`No controls were found in the provide directory: ${flags.controlsDir}`)
-            throw new Error(`No controls were found in the provide directory: ${flags.controlsDir}`)
-          } else {
-            logger.debug(`Found controls in the controls directory files.length is: ${files.length}`)
-            if (flags.backupControls) {
-              let oldControlsDir = path.join(flags.controlsDir, 'oldControls')
-              if (!fs.existsSync(oldControlsDir)) {
-                fs.mkdirSync(oldControlsDir)
-              }
+        } else if (files.length) {
+          logger.debug(`Found controls in the controls directory files.length is: ${files.length}`)
+          if (flags.backupControls) {
+            const oldControlsDir = path.join(flags.controlsDir, 'oldControls')
+            if (!fs.existsSync(oldControlsDir)) {
+              fs.mkdirSync(oldControlsDir)
             }
           }
+        } else {
+          // directory appears to be empty
+          logger.error(`No controls were found in the provide directory: ${flags.controlsDir}`)
+          throw new Error(`No controls were found in the provide directory: ${flags.controlsDir}`)
         }
-      });
+      })
+    } else {
+      throw new Error('Controls folder not specified or does not exist')
     }
 
     logger.debug(`Processing XCCDF Benchmark file: ${flags.xccdfXmlFile} using rule id.`)
-    let profile: Profile
     const xccdf = fs.readFileSync(flags.xccdfXmlFile, 'utf8')
+    /* eslint-disable prefer-const, max-depth */
+    let profile: Profile
     profile = processXCCDF(xccdf, false, 'rule')
-   
-    // Create a map with: key = legacy id (v number) and value = new id (SV number)
-    let controlsMap = new Map();
+
+    // Create a map with: key = legacy id (v or SV number) and value = new id (SV number)
+    const controlsMap = new Map()
     profile.controls.forEach(control => {
-      let controlId = control.tags.legacy?.map((value) => {
-        let control = value.match(/^V-\d+/)?.toString()
-        return (control == undefined) ? '':control
-      }).filter(Boolean)[0]; 
+      const controlId = control.tags.legacy?.map(value => {
+        // let control: string | undefined
+        const control = flags.controlPrefix === 'V' ? value.match(/^V-\d+/)?.toString() : value.match(/^SV-\d+/)?.toString()
+        return (control === undefined) ? '' : control
+      }).find(Boolean)
 
-      //console.log("TAG ID: ", control.id)
-      //console.log("TAG LEGACY: ", controlId)
-      //console.log("----------------------------------------")
-      
       controlsMap.set(controlId, control.id)
-    });
-
-    //console.log("profile length is: ", profile.controls.length)
-    //console.log("New SV number for V-92979 is: ", controlsMap.get('V-92979'))
+    })
 
     logger.debug(`Processing controls directory: ${flags.controlsDir} and updating controls file name and Id.`)
-    let ext = '.rb'
+    const ext = '.rb'
     let processed = 0
     let skipped = 0
-    let tasks: any[] = []
-    let controlsDir = flags.controlsDir
-    const files = await readdir(controlsDir);
+    const controlsDir = flags.controlsDir
+    const files = await readdir(controlsDir)
+
+    // Iterate trough all files processing ony control files, have a .rb extension
+    const controlsProcessedMap = new Map()
     for (const file of files) {
-      const fileExt = path.extname(file);
+      const fileExt = path.extname(file)
       if (fileExt === ext) {
-        // console.log("found file: ", path.parse(file).name)
-        // console.log("New SV number for is: ", controlsMap.get(path.parse(file).name))
-        let oldControlNumber = path.parse(file).name
-        let newControlNumber = controlsMap.get(path.parse(file).name)
-        // console.log("oldControlNumber is: ", oldControlNumber)
-        // console.log("newControlNumber is: ", newControlNumber)
-        // console.log("File is: ", path.join(controlsDir,file))
-        // console.log("----------------------------------------")
-        if (newControlNumber == undefined) {
-          skipped ++
-          console.log("No SV number found, skipping file: ", file)
+        const oldControlNumber = path.parse(file).name
+        const newControlNumber = controlsMap.get(path.parse(file).name)
+        if (newControlNumber === undefined) {
+          skipped++
+          console.log('No SV number found, skipping file:', file)
         } else {
-          let filePath = path.join(controlsDir,file)
-          tasks.push(fs.readFile(filePath, 'utf-8', (err, contents) => {
-            if(err) throw err;
-            const updated = contents.replace(`${oldControlNumber}`, `${newControlNumber}`)
-            let newFileName = path.join(controlsDir, newControlNumber+'.rb')
-            tasks.push(fs.writeFile(newFileName, updated, 'utf-8', err2 => {
-              if (err2) {
-                console.log(err2)
-              } else {
-                processed ++
-                console.log("Updated fie: ", filePath)
-                // move old control to oldControls folder
-                tasks.push(fs.rename(filePath, path.resolve(path.join(controlsDir,'oldControls'), oldControlNumber+'.rb') , (err)=>{
-                  if(err) throw err;
-                  //else console.log('Successfully moved old control: ', oldControlNumber+'.rb');
-                }))
-              }
-            }))
-          }))
+          const filePath = path.join(controlsDir, file)
+          // Read the control content
+          const controlData = fs.readFileSync(filePath, {encoding: 'utf8', flag: 'r'})
+          // Change the V or SV Id to the SV Id
+          const updatedControl = controlData.replace(`${oldControlNumber}`, `${newControlNumber}`)
+          controlsProcessedMap.set(newControlNumber, 'processed')
+          const newFileName = path.join(controlsDir, newControlNumber + '.rb')
+          // Save new file
+          fs.writeFileSync(newFileName, updatedControl)
+          processed++
+          // Move old control to oldControls folder
+          if (flags.backupControls) {
+            fs.renameSync(filePath, path.resolve(path.join(controlsDir, 'oldControls'), oldControlNumber + '.rb'))
+          // Deleted old file
+          } else {
+            try {
+              fs.unlinkSync(filePath)
+            } catch (error: any) {
+              logger.error(`ERROR: Unable to deleted old file ${filePath} because: ${error}`)
+              throw error
+            }
+          }
         }
       }
     }
-    Promise.all(tasks)
-      .then(res => {
-          console.log(`Total processed files: ${processed}`)
-          console.log(`Total skipped files: ${skipped}`)
-        }
-      )
-      .catch(err => console.log(err));
+
+    let newControls = 0
+    let newControlsId = ''
+    for (const newControl of controlsMap.values()) {
+      if (!controlsProcessedMap.has(newControl)) {
+        newControls++
+        newControlsId += newControl + ' '
+      }
+    }
+
+    console.log(`Total skipped files: ${skipped}`)
+    console.log(`Total processed files: ${processed}`)
+    console.log(`Total new controls found in the XCCDF guidance: ${newControls}`)
+    console.log(`New control(s) found: ${newControlsId}`)
   }
 }
