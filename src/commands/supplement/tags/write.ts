52d727c1c0de21806b4c407cdd1f9c3387ea23a4
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2023-05-03 12:13:05 -0400

    Added tags write functionality
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- src/commands/supplement/tags/write.ts
+++ src/commands/supplement/tags/write.ts
@@ -2,51 +2,63 @@ import {Command, Flags} from '@oclif/core'
 import {ExecJSON} from 'inspecjs'
 import fs from 'fs'
 
-export default class WritePassthrough extends Command {
-    static usage = 'supplement passthrough write -i <input-hdf-json> (-f <input-passthrough-json> | -d <passthrough-json>) [-o <output-hdf-json>]'
+export default class WriteTags extends Command {
+    static usage = 'supplement tags write -i <input-hdf-json> (-f <input-tags-json> | -d <tags-json>) [-o <output-hdf-json>]'
 
-    static summary = 'Overwrite the `passthrough` attribute in a given HDF file with the provided `passthrough` JSON data'
+    static summary = 'Overwrite the `tags` attribute in a given HDF file with the provided `tags` JSON data'
 
-    static description = 'Passthrough data can be any context/structure. See sample ideas at https://github.com/mitre/saf/wiki/Supplement-HDF-files-with-additional-information-(ex.-%60passthrough%60,-%60target%60)'
+    static description = 'Tags data can be any context/structure. See sample ideas at https://github.com/mitre/saf/wiki/Supplement-HDF-files-with-additional-information-(ex.-%60tags%60,-%60target%60)'
 
     static examples = [
-      'saf supplement passthrough write -i hdf.json -d \'{"a": 5}\'',
-      'saf supplement passthrough write -i hdf.json -f passthrough.json -o new-hdf.json',
+      'saf supplement tags write -i hdf.json -d \'{"a": 5}\'',
+      'saf supplement tags write -i hdf.json -f tags.json -o new-hdf.json',
     ]
 
     static flags = {
       help: Flags.help({char: 'h'}),
       input: Flags.string({char: 'i', required: true, description: 'An input Heimdall Data Format file'}),
-      passthroughFile: Flags.string({char: 'f', exclusive: ['passthroughData'], description: 'An input passthrough-data file (can contain any valid JSON); this flag or `passthroughData` must be provided'}),
-      passthroughData: Flags.string({char: 'd', exclusive: ['passthroughFile'], description: 'Input passthrough-data (can be any valid JSON); this flag or `passthroughFile` must be provided'}),
+      tagsFile: Flags.string({char: 'f', exclusive: ['tagsData'], description: 'An input tags-data file (can contain any valid JSON); this flag or `tagsData` must be provided'}),
+      tagsData: Flags.string({char: 'd', exclusive: ['tagsFile'], description: 'Input tags-data (can be any valid JSON); this flag or `tagsFile` must be provided'}),
       output: Flags.string({char: 'o', description: 'An output Heimdall Data Format JSON file (otherwise the input file is overwritten)'}),
     }
 
     async run() {
-      const {flags} = await this.parse(WritePassthrough)
+      const {flags} = await this.parse(WriteTags)
 
-      const input: ExecJSON.Execution & {passthrough?: unknown} = JSON.parse(fs.readFileSync(flags.input, 'utf8'))
+      const input: ExecJSON.Execution & {tags?: unknown} = JSON.parse(fs.readFileSync(flags.input, 'utf8'))
       const output: string = flags.output || flags.input
 
-      let passthrough: unknown
-      if (flags.passthroughFile) {
+      let tags: any
+      if (flags.tagsFile) {
         try {
-          passthrough = JSON.parse(fs.readFileSync(flags.passthroughFile, 'utf8'))
+          tags = JSON.parse(fs.readFileSync(flags.tagsFile, 'utf8'))
         } catch (error: unknown) {
-          throw new Error(`Couldn't parse passthrough data: ${error}`)
+          throw new Error(`Couldn't parse tags data: ${error}`)
         }
-      } else if (flags.passthroughData) {
+      } else if (flags.tagsData) {
         try {
-          passthrough = JSON.parse(flags.passthroughData)
+          tags = JSON.parse(flags.tagsData)
         } catch {
-          passthrough = flags.passthroughData
+          tags = flags.tagsData
         }
       } else {
-        throw new Error('One out of passthroughFile or passthroughData must be passed')
+        throw new Error('One out of tagsFile or tagsData must be passed')
       }
 
-      input.passthrough = passthrough
+      // Check for num of keys and type of objects
+      if (Object.keys(input.profiles[0].controls).length !== Object.keys(tags[0]).length || typeof input.profiles[0].controls !== typeof tags[0]) {
+        throw new TypeError('Structure of tags data is invalid')
+      }
+
+      // Overwrite tags
+      input.profiles[0].controls.forEach(control => {
+        const matchingTag = tags[0].find((tag: { gid: string }) => tag.gid === control.id)
+        if (matchingTag !== undefined) {
+          control.tags = matchingTag
+        }
+      })
 
       fs.writeFileSync(output, JSON.stringify(input, null, 2))
+      console.log('Tags successfully overwritten')
     }
 }
