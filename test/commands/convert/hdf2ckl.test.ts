6b60602723ff38bcf20672864238a8ff948afb25
Vanessa Fotso <vfotso@mitre.org>
2023-06-14 11:15:29 -0400

    update test for hdf2ckl converter
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- test/commands/convert/hdf2ckl.test.ts
+++ test/commands/convert/hdf2ckl.test.ts
@@ -9,8 +9,17 @@ describe('Test hdf2checklist', () => {
 
   test
     .stdout()
-    .command(['convert hdf2ckl', '-i', path.resolve('./test/sample_data/HDF/input/vSphere8_report.json'), '-o', `${tmpobj.name}/hdf2ckl_test.json`])
+    .command(['convert hdf2ckl', '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), '-o', `${tmpobj.name}/hdf2ckl_test.json`])
     .it('hdf-converter output test - defaults', () => {
+      const test = fs.readFileSync(`${tmpobj.name}/hdf2ckl_test.json`, 'utf8')
+      const sample = fs.readFileSync(path.resolve('./test/sample_data/checklist/red_hat_good.ckl'), 'utf8')
+      expect(omitChecklistChangingFields(test)).to.eql(omitChecklistChangingFields(sample))
+    })
+
+  test
+    .stdout()
+    .command(['convert hdf2ckl', '-i', path.resolve('./test/sample_data/HDF/input/vSphere8_report.json'), '-o', `${tmpobj.name}/hdf2ckl_test.json`])
+    .it('hdf-converter output test - inspec results from profile with dependent profiles', () => {
       const test = fs.readFileSync(`${tmpobj.name}/hdf2ckl_test.json`, 'utf8')
       const sample = fs.readFileSync(path.resolve('./test/sample_data/checklist/vSphere8_report.ckl'), 'utf8')
       expect(omitChecklistChangingFields(test)).to.eql(omitChecklistChangingFields(sample))
