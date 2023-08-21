4fb1aa4916234d9c7cf7ee9983d4b884c5e8e932
Amndeep Singh Mann <amann@mitre.org>
2023-08-20 20:40:51 -0400

    CAAT mapper (#1686)
    * fixed up usage string convention w/r to a parameter having the 'multiple' attribute set
    
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>
    
    * hdf2caat mapper completed
    
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>
    
    * documentation
    
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>
    
    * typo fix
    
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>
    
    * nitpick
    
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>
    
    * added test
    
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>
    
    ---------
    
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- src/commands/convert/hdf2xccdf.ts
+++ src/commands/convert/hdf2xccdf.ts
@@ -6,14 +6,14 @@ import {default as files} from '../../resources/files.json'
 export default class HDF2XCCDF extends Command {
   static usage = 'convert hdf2xccdf -i <hdf-scan-results-json> -o <output-xccdf-xml> [-h]'
 
-  static description = 'Translate an HDF file into an XCCDF XML'
+  static description = 'Translate an HDF file into an XCCDF XML file'
 
   static examples = ['saf convert hdf2xccdf -i hdf_input.json -o xccdf-results.xml']
 
   static flags = {
     help: Flags.help({char: 'h'}),
-    input: Flags.string({char: 'i', required: true, description: 'Input HDF file'}),
-    output: Flags.string({char: 'o', required: true, description: 'Output HDF JSON File'}),
+    input: Flags.string({char: 'i', required: true, description: 'Input HDF JSON file'}),
+    output: Flags.string({char: 'o', required: true, description: 'Output XCCDF XML file'}),
   }
 
   async run() {
