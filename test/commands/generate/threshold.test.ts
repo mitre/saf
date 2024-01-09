f5cfb853529d77d539109d47ac97ea73cb24ad6a
George M Dias <GDIAS@MITRE.ORG>
2024-01-09 16:37:52 -0600

    1.3.2
    Signed-off-by: George M Dias <GDIAS@MITRE.ORG>

--- test/commands/generate/threshold.test.ts
+++ test/commands/generate/threshold.test.ts
@@ -13,8 +13,8 @@ describe('Generate threshold', () => {
     .it('when provided an output file to store threshold', () => {
       // Need to convert to linefeed (\n) otherwise the test will fail when executed in a Windows platform.
       // The YAML.stringify will always include \n as the last character, as is expected of YAML documents.
-      const test = YAML.stringify(fs.readFileSync(`${tmpobj.name}/red_hat_good.counts.good.yml`, 'utf8').replace(/\r\n/gi, '\n'))
-      const sample = YAML.stringify(fs.readFileSync(path.resolve('./test/sample_data/thresholds/red_hat_good.counts.good.yml'), 'utf8').replace(/\r\n/gi, '\n'))
+      const test = YAML.stringify(fs.readFileSync(`${tmpobj.name}/red_hat_good.counts.good.yml`, 'utf8').replaceAll(/\r\n/gi, '\n'))
+      const sample = YAML.stringify(fs.readFileSync(path.resolve('./test/sample_data/thresholds/red_hat_good.counts.good.yml'), 'utf8').replaceAll(/\r\n/gi, '\n'))
       expect(test).to.eql(sample)
     })
 
@@ -23,6 +23,6 @@ describe('Generate threshold', () => {
     .command(['generate threshold', '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json')])
     .it('when not provided an output file to store threshold', output => {
       // Need to convert to linefeed (\n) otherwise the test will fail when executed in a Windows platform.
-      expect(output.stdout.replace(/\r\n/gi, '\n')).to.contain(fs.readFileSync(path.resolve('./test/sample_data/thresholds/red_hat_good.counts.good.yml'), 'utf8').replace(/\r\n/gi, '\n'))
-    })  
+      expect(output.stdout.replaceAll(/\r\n/gi, '\n')).to.contain(fs.readFileSync(path.resolve('./test/sample_data/thresholds/red_hat_good.counts.good.yml'), 'utf8').replaceAll(/\r\n/gi, '\n'))
+    })
 })
