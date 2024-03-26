2d6c36dd9afa03fe457e339bbd4fdb21c20b0881
DMedina6 <158589619+DMedina6@users.noreply.github.com>
2024-03-26 14:25:43 -0400

    Attestation Create Text Adjustment (#2351)
    * modify CLI prompt to align with implementation
    
    * adjust
    
    * change wording for clarity
    
    * update frequency prompt to new standard
    
    * additional clarity
    
    * text adjust to new implementation
    
    ---------
    
    Co-authored-by: Emily Rodriguez <32680215+em-c-rod@users.noreply.github.com>

--- src/commands/attest/create.ts
+++ src/commands/attest/create.ts
@@ -57,7 +57,7 @@ export default class CreateAttestations extends Command {
       return {
         control_id: id,
         explanation: this.promptForever('Attestation explanation: '),
-        frequency: this.promptForever('Frequency (1d/3d/1wk/2wk/1m/3m/6m/1y/1.5y/custom): '),
+        frequency: this.promptForever('Attestation valid for (enter a number followed by d/w/m/y to indicate days, weeks, months, or years, e.g. 1d/6w/3m/1y/1.5y/custom):'),
         status: this.getStatus(),
         updated: new Date().toISOString(),
         updated_by: this.promptForever('Updated By: '),
