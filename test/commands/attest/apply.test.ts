a71f80b2bc08fae5d5bb14d89d188835d96b71f1
George M Dias <gdias@mitre.org>
2024-03-28 15:24:23 -0500

    Converted generated end-of-files to ensure proper comparison.
    Signed-off-by: George M Dias <gdias@mitre.org>

--- test/commands/attest/apply.test.ts
+++ test/commands/attest/apply.test.ts
@@ -7,13 +7,14 @@ import {omitHDFChangingFields} from '../utils'
 describe('Test attest apply', () => {
   const tmpobj = tmp.dirSync({unsafeCleanup: true})
 
+  // NOTE: replacing all CR from the files being generated to ensure proper comparison.
   test
     .stdout()
     .stderr()
     .command(['attest apply', '-i', path.resolve('./test/sample_data/attestations/rhel8_sample_oneOfEachControlStatus.json'), path.resolve('./test/sample_data/attestations/attestations_jsonFormat.json'), '-o', `${tmpobj.name}/rhel8_attestations_jsonOutput.json`])
     .it('Successfully applies a JSON attestations file', () => {
-      const output = JSON.parse(fs.readFileSync(`${tmpobj.name}/rhel8_attestations_jsonOutput.json`, 'utf8'))
-      const expected = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/attestations/rhel8_sample_oneOfEachControlStatus_output.json'), 'utf8'))
+      const output = JSON.parse(fs.readFileSync(`${tmpobj.name}/rhel8_attestations_jsonOutput.json`, 'utf8').replaceAll(/\r/gi, ''))
+      const expected = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/attestations/rhel8_sample_oneOfEachControlStatus_output.json'), 'utf8').replaceAll(/\r/gi, ''))
 
       expect(omitHDFChangingFields(output)).to.eql(omitHDFChangingFields(expected))
     })
@@ -22,18 +23,18 @@ describe('Test attest apply', () => {
     .stdout()
     .command(['attest apply', '-i', path.resolve('./test/sample_data/attestations/rhel8_sample_oneOfEachControlStatus.json'), path.resolve('./test/sample_data/attestations/attestations_xlsxFormat.xlsx'), '-o', `${tmpobj.name}/rhel8_attestations_xlsxOutput.json`])
     .it('Successfully applies an XLSX attestations file', () => {
-      const output = JSON.parse(fs.readFileSync(`${tmpobj.name}/rhel8_attestations_xlsxOutput.json`, 'utf8'))
-      const expected = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/attestations/rhel8_sample_oneOfEachControlStatus_output.json'), 'utf8'))
+      const output = JSON.parse(fs.readFileSync(`${tmpobj.name}/rhel8_attestations_xlsxOutput.json`, 'utf8').replaceAll(/\r/gi, ''))
+      const expected = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/attestations/rhel8_sample_oneOfEachControlStatus_output.json'), 'utf8').replaceAll(/\r/gi, ''))
 
       expect(omitHDFChangingFields(output)).to.eql(omitHDFChangingFields(expected))
     })
 
-    test
+  test
     .stdout()
     .command(['attest apply', '-i', path.resolve('./test/sample_data/attestations/rhel8_sample_oneOfEachControlStatus.json'), path.resolve('./test/sample_data/attestations/attestations_yamlFormat.yaml'), '-o', `${tmpobj.name}/rhel8_attestations_yamlOutput.json`])
     .it('Successfully applies a YAML attestations file', () => {
-      const output = JSON.parse(fs.readFileSync(`${tmpobj.name}/rhel8_attestations_yamlOutput.json`, 'utf8'))
-      const expected = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/attestations/rhel8_sample_oneOfEachControlStatus_output.json'), 'utf8'))
+      const output = JSON.parse(fs.readFileSync(`${tmpobj.name}/rhel8_attestations_yamlOutput.json`, 'utf8').replaceAll(/\r/gi, ''))
+      const expected = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/attestations/rhel8_sample_oneOfEachControlStatus_output.json'), 'utf8').replaceAll(/\r/gi, ''))
 
       expect(omitHDFChangingFields(output)).to.eql(omitHDFChangingFields(expected))
     })
