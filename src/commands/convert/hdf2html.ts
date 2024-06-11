cebb9187c2e173dbae52ba989a56b7456627dd24
kemley76 <kemley@mitre.org>
2024-06-11 09:29:41 -0400

    update README
    Signed-off-by: kemley76 <kemley@mitre.org>
    
    Signed-off-by: kemley76 <kemley@mitre.org>

--- src/commands/convert/hdf2html.ts
+++ src/commands/convert/hdf2html.ts
@@ -2,6 +2,7 @@ import {Command, Flags} from '@oclif/core'
 import fs from 'fs'
 import path from 'path'
 import {FromHDFToHTMLMapper as Mapper} from '@mitre/hdf-converters'
+import _ from 'lodash'
 
 // All selectable export types for an HTML export
 enum FileExportTypes {
@@ -15,14 +16,15 @@ export default class HDF2HTML extends Command {
 
   static description = 'Translate an HDF file into a Heimdall Report HTML file'
 
-  static examples = ['saf convert hdf2html -i hdf_input.json -o report.html -t Manager']
+  static examples = ['saf convert hdf2html -i hdf_input.json -o report.html -t manager']
 
   static flags = {
     help: Flags.help({char: 'h'}),
     input: Flags.string({char: 'i', required: true, multiple: true, description: 'Input HDF JSON file'}),
     output: Flags.string({char: 'o', required: true, description: 'Output HTML file'}),
-    type: Flags.string({char: 't', default: FileExportTypes.Administrator, 
-    description: 'The report type to generate\nReport types differ with the information they include\nExecutive: Profile Info + Statuses + Compliance Level\nManager: Executive + Compliance Level + Test Results and Details\nAdministrator: Manager + Test Code', options: ['Executive', 'Manager', 'Administrator']})
+    type: Flags.string({char: 't', default: FileExportTypes.Administrator,
+      description: 'The report type to generate\nReport types differ with the information they include\nExecutive: Profile Info + Statuses + Compliance Level\nManager: Executive + Test Results and Details\nAdministrator: Manager + Test Code',
+      options: ['executive', 'manager', 'administrator']}),
   }
 
   async run() {
@@ -40,7 +42,7 @@ export default class HDF2HTML extends Command {
       files.push({data, fileName, fileID})
     }
 
-    const converter = await new Mapper(files, FileExportTypes.Administrator).toHTML()
+    const converter = await new Mapper(files, _.startCase(flags.type) as FileExportTypes).toHTML()
     fs.writeFileSync(flags.output, converter)
   }
 }
