2cae13f2bef48705bb3d0f04ed6461faadf15a45
Charles Hu <computerscience@verizon.net>
2024-07-02 08:41:26 -0400

    GoSec convert command update
    Signed-off-by: Charles Hu <computerscience@verizon.net>

--- test/commands/convert/gosec2hdf.test.ts
+++ test/commands/convert/gosec2hdf.test.ts
@@ -16,3 +16,16 @@ describe('Test gosec', () => {
       expect(omitHDFChangingFields(test)).to.eql(omitHDFChangingFields(sample))
     })
 })
+
+describe('Test gosec using withraw flag', () => {
+  const tmpobj = tmp.dirSync({unsafeCleanup: true})
+
+  test
+    .stdout()
+    .command(['convert gosec2hdf', '-i', path.resolve('./test/sample_data/gosec/sample_input_report/Grype_gosec_results.json'), '-o', `${tmpobj.name}/gosectest.json`, '-w'])
+    .it('hdf-converter output test', () => {
+      const test = JSON.parse(fs.readFileSync(`${tmpobj.name}/gosectest.json`, 'utf8'))
+      const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/gosec/gosec-hdf-withraw.json'), 'utf8'))
+      expect(omitHDFChangingFields(test)).to.eql(omitHDFChangingFields(sample))
+    })
+})
