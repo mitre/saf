e540f794b6f8c53bb4babb5b698af0b62d19d441
Kaden Emley <kemley@mitre.org>
2024-07-24 13:45:42 -0400

    add flags for all metadata fields on hdf2ckl command
    Signed-off-by: Kaden Emley <kemley@mitre.org>

--- test/commands/convert/hdf2ckl.test.ts
+++ test/commands/convert/hdf2ckl.test.ts
@@ -28,7 +28,22 @@ describe('Test hdf2checklist', () => {
   test
     .stdout()
     .command(['convert hdf2ckl', '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), '-o', `${tmpobj.name}/hdf2ckl_metadata_test.ckl`, '-m', path.resolve('./test/sample_data/checklist/metadata.json')])
-    .it('hdf-converter output test - with metadata', () => {
+    .it('hdf-converter output test - with metadata file', () => {
+      const test = fs.readFileSync(`${tmpobj.name}/hdf2ckl_metadata_test.ckl`, 'utf8')
+      const sample = fs.readFileSync(path.resolve('./test/sample_data/checklist/red_hat_good_metadata.ckl'), 'utf8')
+      expect(omitChecklistChangingFields(test)).to.eql(omitChecklistChangingFields(sample))
+    })
+
+  test
+    .stdout()
+    .command(['convert hdf2ckl', '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), 
+      '-o', `${tmpobj.name}/hdf2ckl_metadata_test.ckl`, 
+      '--profilename', 'Red Hat Enterprise Linux 7 STIG', '--version', '2', 
+      '--releasenumber', '6', '--releasedate', '2024/06/08', 
+      '--marking', 'CUI', '--hostname', 'localhost', 
+      '--ip', '127.0.0.1', '--role', 'Domain Controller', 
+      '--assettype', 'Computing', '--techarea', 'Other Review'])
+    .it('hdf-converter output test - with metadata flags', () => {
       const test = fs.readFileSync(`${tmpobj.name}/hdf2ckl_metadata_test.ckl`, 'utf8')
       const sample = fs.readFileSync(path.resolve('./test/sample_data/checklist/red_hat_good_metadata.ckl'), 'utf8')
       expect(omitChecklistChangingFields(test)).to.eql(omitChecklistChangingFields(sample))
@@ -46,11 +61,22 @@ describe('Test hdf2checklist', () => {
   test
     .stdout()
     .stderr()
-    .command(['convert hdf2ckl', '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), '-o', `${tmpobj.name}/hdf2ckl_metadata_error_test.json`, '--ip', 'bad ip address', '-m', path.resolve('test/sample_data/checklist/sample_input_report/invalid_metadata.json')])
+    .command(['convert hdf2ckl', '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), '-o', `${tmpobj.name}/hdf2ckl_metadata_error_test.json`, '-m', path.resolve('test/sample_data/checklist/sample_input_report/invalid_metadata.json')])
     .it(
       'hdf-converter output test - throws error when using invalid checklist metadata',
       ctx => {
-        expect(ctx.stderr).to.equal('Error creating checklist:\nInvalid checklist metadata fields: Host IP (bad ip address), Asset Type (Not a real assettype)\n')
+        expect(ctx.stderr).to.equal('Error creating checklist:\nInvalid checklist metadata fields: Host IP (invalid), Asset Type (Not a real assettype)\n')
+      },
+    )
+
+  test
+    .stdout()
+    .stderr()
+    .command(['convert hdf2ckl', '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), '-o', `${tmpobj.name}/hdf2ckl_metadata_error_test.json`, '--ip', 'bad ip address'])
+    .it(
+      'hdf-converter output test - throws error when using invalid checklist metadata',
+      ctx => {
+        expect(ctx.stderr).to.equal('Error creating checklist:\nInvalid checklist metadata fields: Host IP (bad ip address)\n')
       },
     )
 })
