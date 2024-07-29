21fe860986858f0aef1543276a148bdba3b618d5
George M Dias <GDIAS@MITRE.ORG>
2024-07-29 12:39:20 -0500

    lint fix
    Signed-off-by: George M Dias <GDIAS@MITRE.ORG>

--- src/utils/emasser/utilities.ts
+++ src/utils/emasser/utilities.ts
@@ -2,7 +2,6 @@ import _ from 'lodash'
 import {Flags} from '@oclif/core'
 import {BooleanFlag, OptionFlag} from '@oclif/core/interfaces'
 
-
 interface CliArgs {
   requestType: string;
   endpoint: string;
