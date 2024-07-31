61e1dff2a3e5bc4f61e04a6c39c2eab59b3e4328
Kaden Emley <kemley@mitre.org>
2024-07-31 11:00:12 -0400

    remove heimdall version when running checklist tests
    Signed-off-by: Kaden Emley <kemley@mitre.org>

--- test/commands/utils.ts
+++ test/commands/utils.ts
@@ -29,6 +29,7 @@ export function omitHDFChangingFields(
 }
 
 export function omitChecklistChangingFields(input: string) {
-  // remove UUIDs
+  // remove UUIDs and the heimdall version
   return input.replaceAll(/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}/gm, '')
+    .replace(/<!--Heimdall Version :: \S+-->/, '')
 }
