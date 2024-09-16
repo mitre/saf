886ff4a9935aebe3d9aa835f5793c0b539bc08a9
andytang99 <atang@mitre.org>
2024-09-16 22:40:07 +0000

    small QOL changes

--- src/commands/convert/anchoregrype2hdf.ts
+++ src/commands/convert/anchoregrype2hdf.ts
@@ -12,7 +12,7 @@ export default class AnchoreGrype2HDF extends Command {
 
   static flags = {
     help: Flags.help({char: 'h'}),
-    input: Flags.string({char: 'i', required: true, description: 'Input ANCHOREGRYPE file'}),
+    input: Flags.string({char: 'i', required: true, description: 'Input Anchore Grype file'}),
     output: Flags.string({char: 'o', required: true, description: 'Output HDF file'}),
     'with-raw': Flags.boolean({char: 'w', required: false}),
   }
@@ -22,6 +22,6 @@ export default class AnchoreGrype2HDF extends Command {
     const input = fs.readFileSync(flags.input, 'utf8')
 
     const converter = new Mapper(input, flags['with-raw'])
-    fs.writeFileSync(checkSuffix(flags.output), JSON.stringify(converter.toHdf()))
+    fs.writeFileSync(checkSuffix(flags.output), JSON.stringify(converter.toHdf(), null, 2))
   }
 }
