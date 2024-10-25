919453efe412433799bdf7fca3f944bc8423ab39
Amndeep Singh Mann <amann@mitre.org>
2024-10-25 18:24:58 -0400

    added checkinput step, made output formatted, transitioned to using the extension of of basecommand, fixed help text
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- src/commands/convert/dependency_track2hdf.ts
+++ src/commands/convert/dependency_track2hdf.ts
@@ -1,17 +1,17 @@
 import {Command, Flags} from '@oclif/core'
 import fs from 'fs'
 import {DependencyTrackMapper as Mapper} from '@mitre/hdf-converters'
-import {checkSuffix} from '../../utils/global'
+import {checkInput, checkSuffix} from '../../utils/global'
+import {BaseCommand} from '../../utils/oclif/baseCommand'
 
-export default class DependencyTrack2HDF extends Command {
-  static readonly usage = 'convert dependency_track2hdf -i <dt-fpf-json> -o <hdf-scan-results-json>'
+export default class DependencyTrack2HDF extends BaseCommand<typeof DependencyTrack2HDF> {
+  static readonly usage = '<%= command.id %> -i <dt-fpf-json> -o <hdf-scan-results-json> [-h] [-w]'
 
-  static readonly description = 'Translate a Dependency-Track output file into an HDF results set'
+  static readonly description = 'Translate a Dependency-Track results JSON file into a Heimdall Data Format JSON file'
 
-  static readonly examples = ['saf convert dependency_track2hdf -i dt-fpf.json -o output-hdf-name.json']
+  static readonly examples = ['<%= config.bin %> <%= command.id %> -i dt-fpf.json -o output-hdf-name.json']
 
   static readonly flags = {
-    help: Flags.help({char: 'h'}),
     input: Flags.string({char: 'i', required: true, description: 'Input Dependency-Track FPF file'}),
     output: Flags.string({char: 'o', required: true, description: 'Output HDF file'}),
     'with-raw': Flags.boolean({char: 'w', required: false}),
@@ -19,9 +19,14 @@ export default class DependencyTrack2HDF extends Command {
 
   async run() {
     const {flags} = await this.parse(DependencyTrack2HDF)
-    const input = fs.readFileSync(flags.input, 'utf8')
+    const data = fs.readFileSync(flags.input, 'utf8')
+    checkInput(
+      {data, filename: flags.input},
+      'dependencyTrack',
+      'Dependency-Track results JSON',
+    )
 
-    const converter = new Mapper(input, flags['with-raw'])
-    fs.writeFileSync(checkSuffix(flags.output), JSON.stringify(converter.toHdf()))
+    const converter = new Mapper(data, flags['with-raw'])
+    fs.writeFileSync(checkSuffix(flags.output), JSON.stringify(converter.toHdf(), null, 2))
   }
 }
