ee9b09fc5c09ec84b9f24e303112269686dbdfd2
Amndeep Singh Mann <amann@mitre.org>
2024-10-24 23:13:11 -0400

    sonarqube says that these ought to be readonly
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- src/commands/convert/dependency_track2hdf.ts
+++ src/commands/convert/dependency_track2hdf.ts
@@ -4,13 +4,13 @@ import {DependencyTrackMapper as Mapper} from '@mitre/hdf-converters'
 import {checkSuffix} from '../../utils/global'
 
 export default class DependencyTrack2HDF extends Command {
-  static usage = 'convert dependency_track2hdf -i <dt-fpf-json> -o <hdf-scan-results-json>'
+  static readonly usage = 'convert dependency_track2hdf -i <dt-fpf-json> -o <hdf-scan-results-json>'
 
-  static description = 'Translate a Dependency-Track output file into an HDF results set'
+  static readonly description = 'Translate a Dependency-Track output file into an HDF results set'
 
-  static examples = ['saf convert dependency_track2hdf -i dt-fpf.json -o output-hdf-name.json']
+  static readonly examples = ['saf convert dependency_track2hdf -i dt-fpf.json -o output-hdf-name.json']
 
-  static flags = {
+  static readonly flags = {
     help: Flags.help({char: 'h'}),
     input: Flags.string({char: 'i', required: true, description: 'Input Dependency-Track FPF file'}),
     output: Flags.string({char: 'o', required: true, description: 'Output HDF file'}),
