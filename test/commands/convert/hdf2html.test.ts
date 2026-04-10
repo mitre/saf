5312ea70467503e8dcc84de6aced3a90bd817959
Amndeep Singh Mann <amann@mitre.org>
2026-04-10 07:18:08 -0400

    rework test to match the new style
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- test/commands/convert/hdf2html.test.ts
+++ test/commands/convert/hdf2html.test.ts
@@ -1,45 +1,50 @@
-import {expect, test} from '@oclif/test'
-import tmp from 'tmp'
-import path from 'path'
-import fs from 'fs'
-import { omitHtmlChangingfields as omitHtmlChangingFields } from '../utils'
+import { runCommand } from '@oclif/test';
+import fs from 'fs';
+import path from 'path';
+import tmp from 'tmp';
+import { describe, expect, it } from 'vitest';
+import { omitHtmlChangingfields as omitHtmlChangingFields } from '../utils';
 
 describe('Test hdf2html triple_overlay_profile_example with default (administrator) report type', () => {
-  const tmpobj = tmp.dirSync({unsafeCleanup: true})
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
 
-  test
-    .stdout()
-    .command(['convert hdf2html', '-i', path.resolve('./test/sample_data/HDF/input/triple_overlay_profile_example.json'), '-o', `${tmpobj.name}/triple_overlay_profile_example.html`])
-    .it('hdf-converter output test', () => {
-      const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/triple_overlay_profile_example.html`, 'utf8'))
-      const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/triple_overlay_profile_example.htm'), 'utf8'))
-      expect(converted).to.eql(sample)
-    })
-})
+  it('hdf-converter output test', async () => {
+    await runCommand<{ name: string }>([
+      'convert hdf2html',
+      '-i', path.resolve('./test/sample_data/HDF/input/triple_overlay_profile_example.json'),
+      '-o', `${tmpobj.name}/triple_overlay_profile_example.html`,
+    ]);
+    const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/triple_overlay_profile_example.html`, 'utf8'));
+    const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/triple_overlay_profile_example.htm'), 'utf8'));
+    expect(converted).to.eql(sample);
+  });
+});
 
 describe('Test hdf2html with manager report type and two input files', () => {
-  const tmpobj = tmp.dirSync({unsafeCleanup: true})
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
 
-  test
-    .stdout()
-    .command(['convert hdf2html', '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), path.resolve('./test/sample_data/HDF/input/vSphere8_report.json'), '-o', `${tmpobj.name}/combined_output.html`, '-t', 'manager'])
-    .it('hdf-converter output test', () => {
-      const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/combined_output.html`, 'utf8'))
-      const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/combined_output.htm'), 'utf8'))
-      expect(converted).to.eql(sample)
-    })
-})
+  it('hdf-converter output test', () => {
+    await runCommand<{ name: string }>([
+      'convert hdf2html',
+      '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), path.resolve('./test/sample_data/HDF/input/vSphere8_report.json'),
+      '-o', `${tmpobj.name}/combined_output.html`, '-t', 'manager',
+    ]);
+    const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/combined_output.html`, 'utf8'));
+    const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/combined_output.htm'), 'utf8'));
+    expect(converted).to.eql(sample);
+  });
+});
 
 describe('Test hdf2html with executive report type', () => {
-  const tmpobj = tmp.dirSync({unsafeCleanup: true})
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
 
-  test
-    .stdout()
-    .command(['convert hdf2html', '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), '-o', `${tmpobj.name}/red_hat_good.html`, '-t', 'executive'])
-    .it('hdf-converter output test', () => {
-      const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/red_hat_good.html`, 'utf8'))
-      const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/red_hat_good.htm'), 'utf8'))
-      expect(converted).to.eql(sample)
-    })
-})
-
+  it('hdf-converter output test', () => {
+    await runCommand<{ name: string }>([
+      'convert hdf2html',
+      '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'),
+      '-o', `${tmpobj.name}/red_hat_good.html`, '-t', 'executive']);
+    const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/red_hat_good.html`, 'utf8'));
+    const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/red_hat_good.htm'), 'utf8'));
+    expect(converted).to.eql(sample);
+  });
+});
