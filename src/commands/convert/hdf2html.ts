ddcf0565847e5f6443927440b4d0432882d53cd3
Amndeep Singh Mann <amann@mitre.org>
2026-04-15 16:45:56 -0400

    update integration to match modern standards and also use the latest version of the mapper
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- src/commands/convert/hdf2html.ts
+++ src/commands/convert/hdf2html.ts
@@ -1,48 +1,30 @@
-import { Command, Flags } from '@oclif/core';
 import fs from 'fs';
 import path from 'path';
-import { FromHDFToHTMLMapper as Mapper } from '@mitre/hdf-converters';
-import _ from 'lodash';
-
-// All selectable export types for an HTML export
-enum FileExportTypes {
-  Executive = 'Executive',
-  Manager = 'Manager',
-  Administrator = 'Administrator',
-}
+import { FileExportTypes, FromHDFToHTMLMapper as Mapper } from '@mitre/hdf-converters';
+import { Command, Flags } from '@oclif/core';
 
 export default class HDF2HTML extends Command {
-  static usage = 'convert hdf2html -i <hdf-scan-results-json>... -o <output-html> [-t <output-type>] [-h]';
+  static readonly usage = 'convert hdf2html -i <hdf-scan-results-json>... -o <output-html> [-t <output-type>] [-h]';
 
-  static description = 'Translate an HDF file into a Heimdall Report HTML file';
+  static readonly description = 'Translate an HDF file into a Heimdall Report HTML file';
 
-  static examples = ['saf convert hdf2html -i hdf_input.json -o report.html -t manager'];
+  static readonly examples = ['saf convert hdf2html -i hdf_input.json -o report.html -t manager'];
 
-  static flags = {
+  static readonly flags = {
     help: Flags.help({ char: 'h' }),
     input: Flags.string({ char: 'i', required: true, multiple: true, description: 'Input HDF JSON file' }),
     output: Flags.string({ char: 'o', required: true, description: 'Output HTML file' }),
     type: Flags.string({ char: 't', default: FileExportTypes.Administrator,
       description: 'The report type to generate\nReport types differ with the information they include\nExecutive: Profile Info + Statuses + Compliance Level\nManager: Executive + Test Results and Details\nAdministrator: Manager + Test Code',
-      options: ['executive', 'manager', 'administrator'] }),
+      options: ['Executive', 'Manager', 'Administrator'] }),
   };
 
   async run() {
     const { flags } = await this.parse(HDF2HTML);
 
-    const files = [];
+    const files = flags.input.map((file, i) => ({ data: fs.readFileSync(file, 'utf8'), fileName: path.basename(file), fileID: `${i}` }));
 
-    let i = 0;
-    for (const file of flags.input) {
-      // Create unique fileID for html reference
-      const fileID = `${i++}`;
-
-      const data = fs.readFileSync(file, 'utf8');
-      const fileName = path.basename(file);
-      files.push({ data, fileName, fileID });
-    }
-
-    const converter = await new Mapper(files, _.startCase(flags.type) as FileExportTypes).toHTML();
+    const converter = await new Mapper(files, FileExportTypes[flags.type as keyof typeof FileExportTypes]).toHTML();
     fs.writeFileSync(flags.output, converter);
   }
 }
