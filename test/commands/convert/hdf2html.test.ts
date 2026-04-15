af397a54adfacfd77023c043bb628bfd7cc7b617
Amndeep Singh Mann <amann@mitre.org>
2026-04-15 16:48:20 -0400

    reenable tests that had been too slow - it might still take ages now but it definitely should be fast enough once the monorepo release is out
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- test/commands/convert/hdf2html.test.ts
+++ test/commands/convert/hdf2html.test.ts
@@ -1,53 +1,54 @@
-import { runCommand } from '@oclif/test';
 import fs from 'fs';
 import path from 'path';
 import tmp from 'tmp';
+import { runCommand } from '@oclif/test';
 import { describe, expect, it } from 'vitest';
 import { omitHtmlChangingfields as omitHtmlChangingFields } from '../utils';
 
-// describe('Test hdf2html triple_overlay_profile_example with default (administrator) report type', () => {
-//   const tmpobj = tmp.dirSync({ unsafeCleanup: true });
-//
-//   it('hdf-converter output test', async () => {
-//     await runCommand<{ name: string }>([
-//       'convert hdf2html',
-//       '-i', path.resolve('./test/sample_data/HDF/input/triple_overlay_profile_example.json'),
-//       '-o', `${tmpobj.name}/triple_overlay_profile_example.html`,
-//     ]);
-//     const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/triple_overlay_profile_example.html`, 'utf8'));
-//     const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/triple_overlay_profile_example.html'), 'utf8'));
-//     expect(converted).to.eql(sample);
-//   });
-// });
+describe('Test hdf2html triple_overlay_profile_example with default (administrator) report type', () => {
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
 
-// describe('Test hdf2html with manager report type and two input files', () => {
-//   const tmpobj = tmp.dirSync({ unsafeCleanup: true });
-//
-//   it('hdf-converter output test', async () => {
-//     await runCommand<{ name: string }>([
-//       'convert hdf2html',
-//       '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), path.resolve('./test/sample_data/HDF/input/vSphere8_report.json'),
-//       '-o', `${tmpobj.name}/combined_output.html`, '-t', 'manager',
-//     ]);
-//     const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/combined_output.html`, 'utf8'));
-//     const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/combined_output.html'), 'utf8'));
-//     expect(converted).to.eql(sample);
-//   });
-// });
+  it('hdf-converter output test', async () => {
+    await runCommand<{ name: string }>([
+      'convert hdf2html',
+      '-i', path.resolve('./test/sample_data/HDF/input/triple_overlay_profile_example.json'),
+      '-o', `${tmpobj.name}/triple_overlay_profile_example.html`,
+    ]);
+    const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/triple_overlay_profile_example.html`, 'utf8'));
+    const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/triple_overlay_profile_example.html'), 'utf8'));
+    expect(converted).to.eql(sample);
+  });
+});
 
-// describe('Test hdf2html with executive report type', () => {
-//   const tmpobj = tmp.dirSync({ unsafeCleanup: true });
-//
-//   it('hdf-converter output test', async () => {
-//     await runCommand<{ name: string }>([
-//       'convert hdf2html',
-//       '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'),
-//       '-o', `${tmpobj.name}/red_hat_good.html`, '-t', 'executive']);
-//     const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/red_hat_good.html`, 'utf8'));
-//     const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/red_hat_good.html'), 'utf8'));
-//     expect(converted).to.eql(sample);
-//   });
-// });
+describe('Test hdf2html with manager report type and two input files', () => {
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
+
+  it('hdf-converter output test', async () => {
+    await runCommand<{ name: string }>([
+      'convert hdf2html',
+      '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'), path.resolve('./test/sample_data/HDF/input/vSphere8_report.json'),
+      '-o', `${tmpobj.name}/combined_output.html`, '-t', 'Manager',
+    ]);
+    const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/combined_output.html`, 'utf8'));
+    const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/combined_output.html'), 'utf8'));
+    expect(converted).to.eql(sample);
+  });
+});
+
+describe('Test hdf2html with executive report type', () => {
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
+
+  it('hdf-converter output test', async () => {
+    await runCommand<{ name: string }>([
+      'convert hdf2html',
+      '-i', path.resolve('./test/sample_data/HDF/input/red_hat_good.json'),
+      '-t', 'Executive',
+      '-o', `${tmpobj.name}/red_hat_good.html`]);
+    const converted = omitHtmlChangingFields(fs.readFileSync(`${tmpobj.name}/red_hat_good.html`, 'utf8'));
+    const sample = omitHtmlChangingFields(fs.readFileSync(path.resolve('./test/sample_data/html/red_hat_good.html'), 'utf8'));
+    expect(converted).to.eql(sample);
+  });
+});
 
 describe('Test hdf2html sonarqube with default (administrator) report type', () => {
   const tmpobj = tmp.dirSync({ unsafeCleanup: true });
