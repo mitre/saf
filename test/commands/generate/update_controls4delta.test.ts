31a94f7e27ee97b448f1d3343fb04f51dcc73c0b
Amndeep Singh Mann <amann@mitre.org>
2026-05-13 00:28:18 -0400

    1) remove the resolve cinc auditor path function which does not actually properly resolve the security hotspot described by SonarQube's javascript:S4036.  what it did was grab the first version of the cinc-auditor binary off of the path and use that the entire time.  the security hotspot says that leveraging the path at all is what is the security concern.  2) instead we tell the user to supply us a path to an inspec or cinc auditor binary.  3) added some tests to validate that everything still works
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- test/commands/generate/update_controls4delta.test.ts
+++ test/commands/generate/update_controls4delta.test.ts
@@ -8,7 +8,7 @@ import { describe, expect, it } from 'vitest';
 describe('Test generate update_controls4delta command', () => {
   // This command updates controls in place, so tests must operate on a temp copy
   // of the fixture controls rather than the checked-in sample files
-  it('should rename legacy controls to the new XCCDF control ids', async () => {
+  it('should rename legacy controls to the new XCCDF control ids using --inspecJsonFile', async () => {
     const tempWorkspace = tmp.dirSync({ unsafeCleanup: true });
     const sourceControlsDir = path.resolve('./test/sample_data/inspec/json/profile_and_controls/windows_server_2019_v1r3_mini_controls');
     const tempControlsDir = path.join(tempWorkspace.name, 'controls');
@@ -38,6 +38,37 @@ describe('Test generate update_controls4delta command', () => {
     expect(fs.existsSync(path.join(tempControlsDir, 'SV-205661.rb'))).to.eql(true);
 
     // The original legacy control filenames should no longer exist
+    expect(fs.existsSync(path.join(tempControlsDir, 'V-93369.rb'))).to.eql(false);
+    expect(fs.existsSync(path.join(tempControlsDir, 'V-93205.rb'))).to.eql(false);
+    expect(fs.existsSync(path.join(tempControlsDir, 'V-93207.rb'))).to.eql(false);
+    expect(fs.existsSync(path.join(tempControlsDir, 'V-93473.rb'))).to.eql(false);
+    expect(fs.existsSync(path.join(tempControlsDir, 'V-93461.rb'))).to.eql(false);
+  });
+
+  it('should rename legacy controls to the new XCCDF control ids using --inspecPath', async () => {
+    const tempWorkspace = tmp.dirSync({ unsafeCleanup: true });
+    const sourceControlsDir = path.resolve('./test/sample_data/inspec/json/profile_and_controls/windows_server_2019_v1r3_mini_controls');
+    const tempControlsDir = path.join(tempWorkspace.name, 'controls');
+
+    fs.cpSync(sourceControlsDir, tempControlsDir, { recursive: true });
+
+    await runCommand<{ name: string }>([
+      'generate update_controls4delta',
+      '-I', 'cinc-auditor',
+      '-X', path.resolve('./test/sample_data/xccdf/stigs/Windows_Server_2019_V3R2_xccdf.xml'),
+      '-c', tempControlsDir,
+      '--no-backupControls',
+    ]);
+
+    const fileCount = fs.readdirSync(tempControlsDir).length;
+    expect(fileCount).to.eql(5);
+
+    expect(fs.existsSync(path.join(tempControlsDir, 'SV-205844.rb'))).to.eql(true);
+    expect(fs.existsSync(path.join(tempControlsDir, 'SV-205845.rb'))).to.eql(true);
+    expect(fs.existsSync(path.join(tempControlsDir, 'SV-205846.rb'))).to.eql(true);
+    expect(fs.existsSync(path.join(tempControlsDir, 'SV-205657.rb'))).to.eql(true);
+    expect(fs.existsSync(path.join(tempControlsDir, 'SV-205661.rb'))).to.eql(true);
+
     expect(fs.existsSync(path.join(tempControlsDir, 'V-93369.rb'))).to.eql(false);
     expect(fs.existsSync(path.join(tempControlsDir, 'V-93205.rb'))).to.eql(false);
     expect(fs.existsSync(path.join(tempControlsDir, 'V-93207.rb'))).to.eql(false);
