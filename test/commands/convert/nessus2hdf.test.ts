4ae8a21b97d364256dbd6efe2f744f0388abf00c
NickShumar <nshumar@mitre.org>
2026-08-24 17:13:10 -0400

    Added testing for fast-XML-parser maxTotalExpansion limit input
    Signed-off-by: NickShumar <nshumar@mitre.org>

--- test/commands/convert/nessus2hdf.test.ts
+++ test/commands/convert/nessus2hdf.test.ts
@@ -5,10 +5,10 @@ import tmp from 'tmp';
 import { describe, expect, it } from 'vitest';
 import { omitHDFChangingFields } from '../utils';
 
-describe('Test Nessus', () => {
-  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
+describe.sequential('Test Nessus', () => {
 
   it('hdf-converter output test', async () => {
+    const tmpobj = tmp.dirSync({ unsafeCleanup: true });
     await runCommand<{ name: string }>([
       'convert nessus2hdf',
       '-i', path.resolve('./test/sample_data/nessus/sample_input_report/sample.nessus'),
@@ -26,5 +26,40 @@ describe('Test Nessus', () => {
     expect(omitHDFChangingFields(test1)).to.eql(omitHDFChangingFields(sample1));
     expect(omitHDFChangingFields(test2)).to.eql(omitHDFChangingFields(sample2));
     expect(omitHDFChangingFields(test3)).to.eql(omitHDFChangingFields(sample3));
+  });
+
+  it('maxTotalExpansions passing test', async () => {
+    const tmpobj = tmp.dirSync({ unsafeCleanup: true });
+    await runCommand<{ name: string }>([
+      'convert nessus2hdf',
+      '-i', path.resolve('./test/sample_data/nessus/sample_input_report/sample.nessus'),
+      '-o', `${tmpobj.name}/nessustest.json`,
+      '-p', '{"processEntities":{"maxTotalExpansions":5000}}',
+    ]);
+
+    const test1 = JSON.parse(fs.readFileSync(`${tmpobj.name}/nessustest-10.0.0.1.json`, 'utf8'));
+    const test2 = JSON.parse(fs.readFileSync(`${tmpobj.name}/nessustest-10.0.0.2.json`, 'utf8'));
+    const test3 = JSON.parse(fs.readFileSync(`${tmpobj.name}/nessustest-10.0.0.3.json`, 'utf8'));
+
+    const sample1 = JSON.parse(fs.readFileSync(path.resolve('test/sample_data/nessus/nessus-hdf-10.0.0.1.json'), 'utf8'));
+    const sample2 = JSON.parse(fs.readFileSync(path.resolve('test/sample_data/nessus/nessus-hdf-10.0.0.2.json'), 'utf8'));
+    const sample3 = JSON.parse(fs.readFileSync(path.resolve('test/sample_data/nessus/nessus-hdf-10.0.0.3.json'), 'utf8'));
+
+    expect(omitHDFChangingFields(test1)).to.eql(omitHDFChangingFields(sample1));
+    expect(omitHDFChangingFields(test2)).to.eql(omitHDFChangingFields(sample2));
+    expect(omitHDFChangingFields(test3)).to.eql(omitHDFChangingFields(sample3));
+  });
+
+  it('maxTotalExpansions fail test', async () => {
+    const tmpobj = tmp.dirSync({ unsafeCleanup: true });  
+    const { stdout, stderr } = await runCommand<{ name: string }>([
+      'convert nessus2hdf',
+      '-i', path.resolve('./test/sample_data/nessus/sample_input_report/sample.nessus'),
+      '-o', `${tmpobj.name}/nessustest.json`,
+      '-p', '{"processEntities":{"maxTotalExpansions":50}}',
+    ]);
+
+    expect(stdout).to.equal('');
+    expect(stderr.trim()).to.equal('Error: [EntityReplacer] Entity expansion count limit exceeded: 51 > 50');
   });
 });
